1081f669df0b1a74a6fabbbad349623834ca4c63
bartek 'basz' szurgot <git@baszerr.eu>
2014-05-10 12:45:25 +0200

    added Threading to build

--- cmake/sources.cmake
+++ cmake/sources.cmake
@@ -1,6 +1,15 @@
-file(GLOB SOURCES_ALL But/*.cpp)
-file(GLOB SOURCES_MT  But/*.mt.cpp)
-file(GLOB SOURCES_UT  But/*.ut.cpp)
+file(GLOB SOURCES_ALL
+        But/*.cpp
+        But/Threading/*.cpp
+    )
+file(GLOB SOURCES_MT
+        But/*.mt.cpp
+        But/Threading/*.mt.cpp
+    )
+file(GLOB SOURCES_UT
+        But/*.ut.cpp
+        But/Threading/*.ut.cpp
+    )
 
 set(SOURCES_LIB ${SOURCES_ALL})
 list(REMOVE_ITEM SOURCES_LIB ${SOURCES_UT} ${SOURCES_MT})
