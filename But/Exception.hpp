dace90f629d53ae95f2712a87e63a906d9e61d0a
bartek 'basz' szurgot <git@baszerr.eu>
2014-05-10 17:16:42 +0200

    fixed issue with added ending semicolons after macro declarations - now user need to add them on her own.

--- But/Exception.hpp
+++ But/Exception.hpp
@@ -13,8 +13,8 @@ protected:
   Exception(char const* file, unsigned line, char const* function, char const* defaultMsg, std::string msg);
 };
 
-#define BUT_DEFINE_EXCEPTION(name, base, msgDef) struct name: public base { name(char const* file, unsigned line, char const* func, std::string msg): base(file, line, func, msgDef, std::move(msg)) { }   name(char const* file, unsigned line, char const* func, char const* derivedMsgDef, std::string msg): base(file, line, func, derivedMsgDef, std::move(msg)) { } };
+#define BUT_DEFINE_EXCEPTION(name, base, msgDef) struct name: public base { name(char const* file, unsigned line, char const* func, std::string msg): base(file, line, func, msgDef, std::move(msg)) { }   name(char const* file, unsigned line, char const* func, char const* derivedMsgDef, std::string msg): base(file, line, func, derivedMsgDef, std::move(msg)) { } }
 
-#define BUT_THROW(type, msgExpr) do{ std::stringstream ss; ss << msgExpr; throw type{__FILE__, __LINE__, BOOST_CURRENT_FUNCTION, ss.str()}; }while(false);
+#define BUT_THROW(type, msgExpr) do { std::stringstream ss; ss << msgExpr; throw type{__FILE__, __LINE__, BOOST_CURRENT_FUNCTION, ss.str()}; } while(false)
 
 }
