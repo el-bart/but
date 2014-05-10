58eae11071d2271d61406a2c749db3a61af2c09d
bartek 'basz' szurgot <git@baszerr.eu>
2014-05-10 19:32:57 +0200

    ActiveObject has been implemented and tested

---
+++ But/Threading/ActiveObject.hpp
@@ -0,0 +1,57 @@
+#pragma once
+#include <memory>
+#include <future>
+#include <thread>
+
+#include "Fifo.hpp"
+#include "JoiningThread.hpp"
+
+namespace But
+{
+namespace Threading
+{
+
+class ActiveObject
+{
+public:
+  ActiveObject();
+  ~ActiveObject();
+
+  template<typename F>
+  auto run(F f)
+  {
+    using Ret = decltype(f());
+    auto cmd = std::make_unique<Task<Ret>>( std::move(f) );
+    auto fut = cmd->task_.get_future();
+    Queue::lock_type lock{q_};
+    q_.push( std::move(cmd) );
+    return fut;
+  }
+
+private:
+  struct Command
+  {
+    virtual ~Command() { }
+    virtual void run() = 0;
+  };
+
+  template<typename R>
+  struct Task: public Command
+  {
+    template<typename F>
+    Task(F f): task_{ std::move(f) } { }
+    virtual void run() noexcept { task_(); }
+    std::packaged_task<R()> task_;
+  };
+
+  void processingLoop() noexcept;
+
+  using Queue = Fifo<std::unique_ptr<Command>>;
+
+  Queue                      q_;
+  std::atomic<bool>          quit_;
+  JoiningThread<std::thread> th_;
+};
+
+}
+}
---
+++ But/Threading/ActiveObject.ut.cpp
@@ -0,0 +1,92 @@
+#include <string>
+#include <chrono>
+
+#include "gtest/gtest.h"
+#include "ActiveObject.hpp"
+
+using ActObj = But::Threading::ActiveObject;
+
+namespace
+{
+
+struct ButThreadingActiveObject: public testing::Test
+{
+  ActObj               ao_;
+  std::chrono::seconds timeout_{5};
+};
+
+
+TEST_F(ButThreadingActiveObject, ClosingRightAway)
+{
+  // leave this empty
+}
+
+
+TEST_F(ButThreadingActiveObject, PassSomethingToProcess)
+{
+  auto f = ao_.run( []{ return std::string{"narf"}; } );
+  ASSERT_TRUE( f.wait_for(timeout_)==std::future_status::ready );
+  EXPECT_EQ( f.get(), "narf" );
+}
+
+
+int getAnswer() { return 42; }
+
+TEST_F(ButThreadingActiveObject, ProcessingFunction)
+{
+  auto f = ao_.run(getAnswer);
+  ASSERT_TRUE( f.wait_for(timeout_)==std::future_status::ready );
+  EXPECT_EQ( f.get(), 42 );
+}
+
+
+struct MovableOnly
+{
+  MovableOnly() = default;
+  MovableOnly(MovableOnly&&) = default;
+  MovableOnly& operator=(MovableOnly&&) = default;
+  int operator()() { return 997; }
+};
+
+TEST_F(ButThreadingActiveObject, ProcessingMovableOnlyFunctor)
+{
+  auto f = ao_.run(MovableOnly{});
+  ASSERT_TRUE( f.wait_for(timeout_)==std::future_status::ready );
+  EXPECT_EQ( f.get(), 997 );
+}
+
+
+TEST_F(ButThreadingActiveObject, ForwardingExceptions)
+{
+  auto f = ao_.run( []()->int { throw std::runtime_error{"kaboom!"}; } );
+  ASSERT_TRUE( f.wait_for(timeout_)==std::future_status::ready );
+  EXPECT_THROW( f.get(), std::runtime_error );
+}
+
+
+template<int N>
+int getSth() { return N; }
+
+TEST_F(ButThreadingActiveObject, MultipleCalls)
+{
+  auto f0 = ao_.run(getSth<0>);
+  auto f1 = ao_.run(getSth<1>);
+  auto f2 = ao_.run(getSth<2>);
+  ASSERT_TRUE( f0.wait_for(timeout_)==std::future_status::ready );
+  EXPECT_EQ( f0.get(), 0 );
+  ASSERT_TRUE( f1.wait_for(timeout_)==std::future_status::ready );
+  EXPECT_EQ( f1.get(), 1 );
+  ASSERT_TRUE( f2.wait_for(timeout_)==std::future_status::ready );
+  EXPECT_EQ( f2.get(), 2 );
+}
+
+
+void doNothing() { }
+
+TEST_F(ButThreadingActiveObject, NoReturnValueDoesNothing)
+{
+  auto f = ao_.run(doNothing);
+  ASSERT_TRUE( f.wait_for(timeout_)==std::future_status::ready );
+}
+
+}
