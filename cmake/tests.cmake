d783aee527b6ead848d6d2003bee3c0806513b43
bartek 'basz' szurgot <git@baszerr.eu>
2014-05-10 16:05:48 +0200

    added main library to linking phase

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -4,6 +4,7 @@ add_executable(uts ${SOURCES_UT})
 add_executable(mts ${SOURCES_MT})
 
 foreach(app mts uts)
+  target_link_libraries(${app} but)
   target_link_libraries(${app} gmock_main gmock gtest)
   set_target_properties(${app} PROPERTIES COMPILE_FLAGS --coverage)
   set_target_properties(${app} PROPERTIES LINK_FLAGS    --coverage)
