d335805b0e920f38d28045e639383553514d8fad
bartek 'basz' szurgot <git@baszerr.eu>
2014-05-10 14:59:07 +0200

    Fifo is mostly implemented and tested

--- But/Threading/Fifo.hpp
+++ But/Threading/Fifo.hpp
@@ -1,10 +1,113 @@
-#ifndef FIFO_HPP
-#define FIFO_HPP
+#pragma once
 
+#include <deque>
+#include <mutex>
+#include <utility>
+#include <condition_variable>
+#include <cassert>
+
+namespace But
+{
+namespace Threading
+{
+
+template<typename T>
 class Fifo
 {
+  using Queue = std::deque<T>;
+
 public:
-  Fifo();
+  using value_type = T;
+  using size_type  = typename Queue::size_type;
+  using lock_type  = std::unique_lock<Fifo<T>>;
+
+  Fifo() = default;
+
+  void lock()
+  {
+    m_.lock();
+    assert(not locked_);
+    locked_ = true;
+  }
+
+  void unlock()
+  {
+    assert(locked_);
+    locked_ = false;
+    m_.unlock();
+  }
+
+  template<typename U>
+  void push(U&& u)
+  {
+    assert(locked_);
+    q_.push_back(std::forward<U>(u));
+    nonEmpty_.notify_all();
+  }
+
+  bool empty() const
+  {
+    assert(locked_);
+    return q_.empty();
+  }
+
+  size_type size() const
+  {
+    assert(locked_);
+    return q_.size();
+  }
+
+  T& top()
+  {
+    assert(locked_);
+    assert( not empty() );
+    return q_.front();
+  }
+
+  T& top(lock_type& lock)
+  {
+    assert(locked_);
+    assert(lock.owns_lock());
+    nonEmpty_.wait(lock, [&]{ return not empty(); });
+    return q_.front();
+  }
+
+  T const& top() const
+  {
+    assert(locked_);
+    assert( not empty() );
+    return q_.front();
+  }
+
+  T const& top(lock_type& lock) const
+  {
+    assert(locked_);
+    assert(lock.owns_lock());
+    nonEmpty_.wait(lock, [&]{ return not empty(); });
+    return q_.front();
+  }
+
+  void pop()
+  {
+    assert(locked_);
+    assert( not empty() );
+    q_.pop_front();
+  }
+
+  void pop(lock_type& lock)
+  {
+    assert(locked_);
+    assert(lock.owns_lock());
+    nonEmpty_.wait(lock, [&]{ return not empty(); });
+    q_.pop_front();
+  }
+
+private:
+  bool                                locked_{false};
+  Queue                               q_;
+  std::mutex                          m_;
+  mutable std::condition_variable_any nonEmpty_;
 };
 
-#endif // FIFO_HPP
+}
+}
--- But/Threading/Fifo.ut.cpp
+++ But/Threading/Fifo.ut.cpp
@@ -1,1 +1,123 @@
+#include <string>
+#include <memory>
+#include <thread>
 
+#include "gtest/gtest.h"
+#include "Fifo.hpp"
+#include "JoiningThread.hpp"
+
+using But::Threading::Fifo;
+using Thread = But::Threading::JoiningThread<std::thread>;
+
+namespace
+{
+
+using Q = Fifo<std::string>;
+
+struct ThreadingFifo: public testing::Test
+{
+  Q q_;
+};
+
+
+TEST_F(ThreadingFifo, DoNothing)
+{
+}
+
+
+TEST_F(ThreadingFifo, PushPopOne)
+{
+  Q::lock_type lock{q_};
+  EXPECT_TRUE(q_.empty());
+
+  q_.push("test");
+  EXPECT_FALSE(q_.empty());
+  ASSERT_EQ(q_.size(), 1u);
+  EXPECT_EQ(q_.top(), "test");
+
+  q_.pop();
+  EXPECT_EQ(q_.size(), 0u);
+  EXPECT_TRUE(q_.empty());
+}
+
+
+TEST_F(ThreadingFifo, Order)
+{
+  Q::lock_type lock{q_};
+  EXPECT_TRUE(q_.empty());
+
+  q_.push("1");
+  q_.push("2");
+  q_.push("3");
+
+  ASSERT_EQ(q_.size(), 3u);
+  EXPECT_EQ(q_.top(), "1");
+  q_.pop();
+
+  ASSERT_EQ(q_.size(), 2u);
+  EXPECT_EQ(q_.top(), "2");
+  q_.pop();
+
+  ASSERT_EQ(q_.size(), 1u);
+  EXPECT_EQ(q_.top(), "3");
+  q_.pop();
+
+  EXPECT_EQ(q_.size(), 0u);
+  EXPECT_TRUE(q_.empty());
+}
+
+
+TEST_F(ThreadingFifo, TopConst)
+{
+  Q::lock_type lock{q_};
+  EXPECT_TRUE(q_.empty());
+
+  q_.push("test");
+  ASSERT_EQ(q_.size(), 1u);
+  EXPECT_EQ( static_cast<Q const&>(q_).top(), "test");
+}
+
+
+TEST_F(ThreadingFifo, MovableOnlyObjects)
+{
+  Fifo<std::unique_ptr<int>> q;
+  decltype(q)::lock_type lock{q};
+  EXPECT_TRUE(q.empty());
+
+  q.push(std::make_unique<int>(42));
+  EXPECT_FALSE(q.empty());
+  ASSERT_EQ(q.size(), 1u);
+  std::unique_ptr<int> p = std::move(q.top());
+  ASSERT_TRUE(p.get()!=nullptr);
+  EXPECT_EQ(*p, 42);
+
+  q.pop();
+  EXPECT_EQ(q.size(), 0u);
+  EXPECT_TRUE(q.empty());
+}
+
+
+TEST_F(ThreadingFifo, TopWaitForElementToTop)
+{
+  Thread th{ [&]{ Q::lock_type lock{q_}; q_.push("narf"); } };
+  Q::lock_type lock{q_};
+  EXPECT_EQ( q_.top(lock), "narf" );
+}
+
+
+TEST_F(ThreadingFifo, TopWaitForElementToTopConst)
+{
+  Thread th{ [&]{ Q::lock_type lock{q_}; q_.push("narf"); } };
+  Q::lock_type lock{q_};
+  EXPECT_EQ( static_cast<Q const&>(q_).top(lock), "narf" );
+}
+
+
+TEST_F(ThreadingFifo, TopWaitForElementToPop)
+{
+  Thread th{ [&]{ Q::lock_type lock{q_}; q_.push("narf"); } };
+  Q::lock_type lock{q_};
+  q_.pop(lock);
+}
+
+}
