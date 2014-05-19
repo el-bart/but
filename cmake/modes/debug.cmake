a909a142e04b4a2f4653b272522a4d0e90ea2f59
bartek 'basz' szurgot <git@baszerr.eu>
2014-05-19 20:59:33 +0200

    added coverage support

--- cmake/modes/debug.cmake
+++ cmake/modes/debug.cmake
@@ -1,4 +1,12 @@
 add_compile_options(-g3 -ggdb)
+
+if("${COVERAGE}" STREQUAL "yes")
+  message(STATUS "enabing coverage counting")
+  add_compile_options(--coverage)
+  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --coverage")
+endif()
+
+add_compile_options(-g3 -ggdb)
 if("${SANITIZE}" STREQUAL "address")
   message(STATUS "enabling address sanitizer")
   add_compile_options(-fsanitize=address)
