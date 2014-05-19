fb34a22e38420a949cede246ae8e6b409a9dd13b
bartek 'basz' szurgot <git@baszerr.eu>
2014-05-19 17:24:30 +0200

    compilation fix

--- But/Log/detail/logImpl.hpp
+++ But/Log/detail/logImpl.hpp
@@ -2,6 +2,7 @@
 
 #include <sstream>
 #include <iostream>
+#include <boost/current_function.hpp>
 
 #include "But/Log/Priority.hpp"
 
@@ -14,7 +15,7 @@ namespace detail
 
 void logProxy(const Priority p, char const* file, unsigned line, char const* function, std::string msg) noexcept;
 
-#define BUT_LOG_DETAIL_LOGIMPL_BODY(pri, msg) std::stringstream ss; ss << msg; ::But::Log::detail::logProxy( ::But::Log::Priority::##pri, __FILE__, __LINE__, BOOST_PRETTY_FUNCTION, ss.str() )
+#define BUT_LOG_DETAIL_LOGIMPL_BODY(pri, msg) std::stringstream ss; ss << msg; ::But::Log::detail::logProxy( ::But::Log::Priority::##pri, __FILE__, __LINE__, BOOST_CURRENT_FUNCTION, ss.str() )
 #define BUT_LOG_DETAIL_LOGIMPL(pri, msg) try { BUT_LOG_DETAIL_LOGIMPL_BODY(pri,msg); } catch(...) { std::cerr << "But::Log: failed to log due to exception" << std::endl; }
 
 }
