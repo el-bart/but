89f97282705e72fbbe467fa4f972920fa3d2d55d
bartek 'basz' szurgot <git@baszerr.eu>
2014-05-19 20:45:27 +0200

    added message indicating that address sanitizer is being enabled

--- cmake/modes/debug.cmake
+++ cmake/modes/debug.cmake
@@ -1,5 +1,6 @@
 add_compile_options(-g3 -ggdb)
 if("${SANITIZE}" STREQUAL "address")
+  message(STATUS "enabling address sanitizer")
   add_compile_options(-fsanitize=address)
   set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=address -lasan")
 endif()
