b30bf3cc116d7525a3c46f8d790dc334fb2ffc84
bartek 'basz' szurgot <git@baszerr.eu>
2016-12-22 22:12:51 +0100

    toolchain files for cmake are now valid

--- src/cmake/toolchains/clang.cmake
+++ src/cmake/toolchains/clang.cmake
@@ -1,9 +1,2 @@
 set(CMAKE_C_COMPILER   clang)
 set(CMAKE_CXX_COMPILER clang++)
-add_compile_options(-Wall -Werror)
-add_compile_options(-std=c++14)
-add_compile_options(-fstack-protector-all)
-
-# use LLVM's libc++ instead of GNU's libstdc++
-#add_compile_options(-stdlib=libc++)
-#set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++")
--- src/cmake/toolchains/gcc.cmake
+++ src/cmake/toolchains/gcc.cmake
@@ -1,5 +1,2 @@
 set(CMAKE_C_COMPILER   gcc)
 set(CMAKE_CXX_COMPILER g++)
-add_compile_options(-Wall -Werror)
-add_compile_options(-std=c++14)
-add_compile_options(-fstack-protector-all)
