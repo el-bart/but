8d6e2f9a6067217dce0ece2fd9a9f9a6f2d13e07
bartek 'basz' szurgot <git@baszerr.eu>
2017-01-02 18:41:51 +0100

    bit-packed sequence under development

---
+++ src/But/Container/BitPackedSequence.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include <vector>
+#include <limits>
+
+namespace But
+{
+namespace Container
+{
+
+/** @brief container-adapter for keeping data bit-wise (as an oposite to typical byte-wise).
+ *
+ *  @warning writing to different elements of the same container may be a data race. just don't do that.
+ *
+ * TODO: finish description
+ *
+ * TODO: provide usage example
+ */
+template<typename T, typename Packer, typename Container = std::vector<uint8_t>>
+class BitPackedSequence final
+{
+  static_assert( Packer::bits_count <= 8u,
+                 "for now up to 8-bits packed values are supported" );
+  static_assert( (1<<(Packer::bits_count-1u)) <= std::numeric_limits<typename Container::value_type>::max(),
+                 "packer produces elements that are bigger than container's single element capacity" );
+
+public:
+  using size_type  = typename Container::size_type;
+  using value_type = T;
+  using iterator       = void*; // TODO
+  using const_iterator = void*; // TODO
+
+  bool empty() const { return c_.empty(); }
+  size_type size() const { return c_.size(); }
+
+  iterator begin() { return nullptr; } // TODO
+  iterator end()   { return nullptr; } // TODO
+
+  const_iterator begin() const { return nullptr; } // TODO
+  const_iterator end()   const { return nullptr; } // TODO
+  const_iterator cbegin() const { return begin(); }
+  const_iterator cend()   const { return end(); }
+
+  void push_back(const value_type v)
+  {
+    // TODO
+  }
+
+private:
+  Container c_;
+};
+
+}
+}
---
+++ src/But/Container/BitPackedSequence.ut.cpp
@@ -0,0 +1,195 @@
+#include <cassert>
+#include "gtest/gtest.h"
+#include "BitPackedSequence.hpp"
+
+using But::Container::BitPackedSequence;
+
+namespace
+{
+
+enum class Elem
+{
+  X = 0,
+  Y = 1,
+  Z = 2
+};
+
+
+struct Packer
+{
+  static constexpr unsigned bits_count = 2;
+
+  static auto encode(const Elem e)
+  {
+    const auto tmp = static_cast<uint8_t>(e);
+    assert( tmp < (1<<(bits_count-1)) );
+    return tmp;
+  }
+
+  static auto decode(uint8_t v)
+  {
+    assert( v <= 2u && "element is over Elem::Z" );
+    return static_cast<Elem>(v);
+  }
+};
+
+
+struct ButContainerBitPackedSequence: public testing::Test
+{
+  using Data = BitPackedSequence<Elem, Packer>;
+  Data        d_;
+  Data const& cd_ = d_;
+};
+
+
+TEST_F(ButContainerBitPackedSequence, UsualTypedefsArePresentAndValid)
+{
+  EXPECT_TRUE( ( std::is_same<Data::size_type, std::vector<Elem>::size_type>::value ) );
+  EXPECT_TRUE( ( std::is_same<Data::value_type, Elem>::value) );
+}
+
+
+TEST_F(ButContainerBitPackedSequence, EmptyContainerIsFine)
+{
+  EXPECT_TRUE( cd_.empty() );
+  EXPECT_EQ( cd_.size(), 0u );
+  EXPECT_TRUE( begin(d_)  == end(d_) );
+  EXPECT_TRUE( begin(cd_) == end(cd_) );
+  EXPECT_TRUE( cbegin(d_) == cend(d_) );
+}
+
+
+TEST_F(ButContainerBitPackedSequence, AddingElementsToContainer)
+{
+  d_.push_back(Elem::Z);
+  d_.push_back(Elem::Y);
+  d_.push_back(Elem::X);
+  EXPECT_EQ( cd_.size(), 3u );
+  // TODO
+}
+
+
+TEST_F(ButContainerBitPackedSequence, RemovingElements)
+{
+  // TODO
+}
+
+
+TEST_F(ButContainerBitPackedSequence, ReplacingElements)
+{
+  // TODO
+}
+
+
+TEST_F(ButContainerBitPackedSequence, RandomAccess)
+{
+  // TODO
+}
+
+
+TEST_F(ButContainerBitPackedSequence, Iterating)
+{
+  // TODO
+}
+
+
+TEST_F(ButContainerBitPackedSequence, ConstIterating)
+{
+  // TODO
+}
+
+
+TEST_F(ButContainerBitPackedSequence, ReverseIterating)
+{
+  // TODO
+}
+
+
+TEST_F(ButContainerBitPackedSequence, ConstReverseIterating)
+{
+  // TODO
+}
+
+
+enum class OddElem
+{
+  X = 0,
+  Y = 1,
+  Z = 2,
+  W = 3,
+  Q = 4
+};
+
+
+struct OddPacker
+{
+  static constexpr unsigned bits_count = 3;
+
+  static auto encode(const OddElem e)
+  {
+    const auto tmp = static_cast<uint8_t>(e);
+    assert( tmp < (1<<(bits_count-1)) );
+    return tmp;
+  }
+
+  static auto decode(uint8_t v)
+  {
+    assert( v <= 3u && "element is over OddElem::Q" );
+    return static_cast<OddElem>(v);
+  }
+};
+
+
+TEST_F(ButContainerBitPackedSequence, WorksWithOddBitCount)
+{
+  using Data = BitPackedSequence<OddElem, OddPacker>;
+  Data d;
+  // TODO
+}
+
+
+enum class MaxElem
+{
+  X = 0u,
+  Y = 255u,
+};
+
+
+struct MaxPacker
+{
+  static constexpr unsigned bits_count = 8;
+
+  static auto encode(const MaxElem e)
+  {
+    const auto tmp = static_cast<uint8_t>(e);
+    assert( tmp < (1<<(bits_count-1)) );
+    return tmp;
+  }
+
+  static auto decode(uint8_t v)
+  {
+    return static_cast<MaxElem>(v);
+  }
+};
+
+
+TEST_F(ButContainerBitPackedSequence, WorksWithUpTo8Bits)
+{
+  using Data = BitPackedSequence<MaxElem, MaxPacker>;
+  Data d;
+  // TODO
+}
+
+
+TEST_F(ButContainerBitPackedSequence, WorksWithDequeAsUnderlyingContainer)
+{
+  // TODO
+}
+
+
+TEST_F(ButContainerBitPackedSequence, WorksWithArrayAsUnderlyingContainer)
+{
+  // TODO
+}
+
+}
