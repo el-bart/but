85ef12ee8aba916322d0047e72087b1af043875b
bartek 'basz' szurgot <git@baszerr.eu>
2017-01-03 20:52:07 +0100

    MaxSizedSequence - WIP

--- src/But/Container/MaxSizedSequence.hpp
+++ src/But/Container/MaxSizedSequence.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <vector>
+#include <type_traits>
+#include <cassert>
 
 namespace But
 {
@@ -10,12 +12,38 @@ template<typename T, size_t N, typename Container = std::vector<T>>
 class MaxSizedSequence final
 {
 public:
+  using size_type  = typename Container::size_type;
+  using value_type = typename Container::value_type;
+
   MaxSizedSequence() = default;
-  // TODO
+
+  bool empty() const { return c_.empty(); }
+  size_type size() const { return c_.size(); }
+  constexpr size_t max_size() const { return N; }
+
+  bool push_back(value_type vt)
+  {
+    if( c_.size() == N )
+      return false;
+    //c_[
+    c_.push_back( std::move(vt) );
+    return true;
+  }
+
+  void pop_back()
+  {
+    assert( not empty() );
+    c_.pop_back();
+  }
+
+
 
 private:
+  static_assert( std::is_same<T, value_type>::value, "container's type does not match declared" );
+
   Container c_;
-  size_t    additions_;
+  size_type size_{0};
+  //size_type additions_;
 };
 
 }
--- src/But/Container/MaxSizedSequence.ut.cpp
+++ src/But/Container/MaxSizedSequence.ut.cpp
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
 #include "MaxSizedSequence.hpp"
 
-using But::Container::MaxSizedSquence;
+using But::Container::MaxSizedSequence;
 
 namespace
 {
@@ -11,12 +11,98 @@ struct ButContainerMaxSizedSequence: public testing::Test
   using Sequence = MaxSizedSequence<std::string, 3>;
 
   Sequence        s_;
-  Sequence const& cs_;
+  Sequence const& cs_ = s_;
 };
 
 
 TEST_F(ButContainerMaxSizedSequence, NewlyCreatedCollectionIsEmpty)
 {
+  EXPECT_TRUE( cs_.empty() );
+  EXPECT_EQ( cs_.size(), 0u );
+  EXPECT_EQ( cs_.max_size(), 3u );
+}
+
+
+TEST_F(ButContainerMaxSizedSequence, AddingElementsThatFit)
+{
+  EXPECT_EQ( cs_.max_size(), 3u );
+
+  EXPECT_TRUE( s_.push_back("one") );
+
+  EXPECT_FALSE( cs_.empty() );
+  EXPECT_EQ( cs_.size(), 1u );
+
+  EXPECT_TRUE( s_.push_back("two") );
+  EXPECT_EQ( cs_.size(), 2u );
+
+  EXPECT_TRUE( s_.push_back("three") );
+  ASSERT_EQ( cs_.size(), 3u );
+
+  // TODO: check content
+}
+
+
+TEST_F(ButContainerMaxSizedSequence, OverflowingDoesNotAddNewElements)
+{
+  EXPECT_TRUE( s_.push_back("one") );
+  EXPECT_TRUE( s_.push_back("two") );
+  EXPECT_TRUE( s_.push_back("three") );
+  EXPECT_FALSE( s_.push_back("four") );
+
+  // TODO: check content
+}
+
+
+TEST_F(ButContainerMaxSizedSequence, ConstIterating)
+{
+  EXPECT_TRUE( s_.push_back("one") );
+  EXPECT_TRUE( s_.push_back("two") );
+
+  // TODO: check content
+}
+
+
+TEST_F(ButContainerMaxSizedSequence, Iterating)
+{
+  EXPECT_TRUE( s_.push_back("one") );
+  EXPECT_TRUE( s_.push_back("two") );
+
+  // TODO: check content
+}
+
+
+TEST_F(ButContainerMaxSizedSequence, OverwritingExistingElement)
+{
+  EXPECT_TRUE( s_.push_back("one") );
+  EXPECT_TRUE( s_.push_back("two") );
+
+  // TODO: check content
+}
+
+
+TEST_F(ButContainerMaxSizedSequence, RemovingElementFromTheEnd)
+{
+  EXPECT_TRUE( s_.push_back("one") );
+  EXPECT_TRUE( s_.push_back("two") );
+  s_.pop_back();
+  EXPECT_EQ( s_.size(), 1u );
+
+  // TODO: check content
+}
+
+
+TEST_F(ButContainerMaxSizedSequence, ArrayWorksWithAdapter)
+{
+  FAIL();
+  /*
+  MaxSizedSequence<std::string, 3, std::array<std::string,3>> s;
+  EXPECT_TRUE( s.push_back("one") );
+  EXPECT_TRUE( s.push_back("two") );
+  s.pop_back();
+  EXPECT_EQ( s.size(), 1u );
+  */
+
+  // TODO: check content
 }
 
 }
