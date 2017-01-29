b40b5a5a21553cd2e506f2ea9beeaf0a9b0be711
bartek 'basz' szurgot <git@baszerr.eu>
2017-01-29 21:09:09 +0100

    added missing inlines for non-template collection operators and removed from template ones (not needed there)

--- src/But/Mpl/FreeOperators.hpp
+++ src/But/Mpl/FreeOperators.hpp
@@ -46,17 +46,17 @@
  */
 #define BUT_MPL_FREE_OPERATORS_TEMPLATE_COMPARE(Class, Extract, ...) \
   template<__VA_ARGS__> \
-  inline bool operator< (Class const& lhs, Class const& rhs) { return lhs Extract <  rhs Extract ; } \
+  bool operator< (Class const& lhs, Class const& rhs) { return lhs Extract <  rhs Extract ; } \
   template<__VA_ARGS__> \
-  inline bool operator<=(Class const& lhs, Class const& rhs) { return lhs Extract <= rhs Extract ; } \
+  bool operator<=(Class const& lhs, Class const& rhs) { return lhs Extract <= rhs Extract ; } \
   template<__VA_ARGS__> \
-  inline bool operator> (Class const& lhs, Class const& rhs) { return lhs Extract >  rhs Extract ; } \
+  bool operator> (Class const& lhs, Class const& rhs) { return lhs Extract >  rhs Extract ; } \
   template<__VA_ARGS__> \
-  inline bool operator>=(Class const& lhs, Class const& rhs) { return lhs Extract >= rhs Extract ; } \
+  bool operator>=(Class const& lhs, Class const& rhs) { return lhs Extract >= rhs Extract ; } \
   template<__VA_ARGS__> \
-  inline bool operator==(Class const& lhs, Class const& rhs) { return lhs Extract == rhs Extract ; } \
+  bool operator==(Class const& lhs, Class const& rhs) { return lhs Extract == rhs Extract ; } \
   template<__VA_ARGS__> \
-  inline bool operator!=(Class const& lhs, Class const& rhs) { return lhs Extract != rhs Extract ; }
+  bool operator!=(Class const& lhs, Class const& rhs) { return lhs Extract != rhs Extract ; }
 
 
 /** @brief helper macro to define all comparison operators, for a given collection type
@@ -74,29 +74,29 @@
  *  </code>
  */
 #define BUT_MPL_FREE_OPERATORS_COLLECTION_COMPARE(Collection) \
-  bool operator==(Collection const& lhs, Collection const& rhs) \
+  inline bool operator==(Collection const& lhs, Collection const& rhs) \
   { \
     using std::begin; \
     using std::end; \
     return std::equal( begin(lhs), end(lhs), begin(rhs), end(rhs) ); \
   } \
-  bool operator!=(Collection const& lhs, Collection const& rhs) \
+  inline bool operator!=(Collection const& lhs, Collection const& rhs) \
   { \
     return not ( lhs == rhs ); \
   } \
-  bool operator<(Collection const& lhs, Collection const& rhs) \
+  inline bool operator<(Collection const& lhs, Collection const& rhs) \
   { \
     return std::lexicographical_compare( lhs.begin(), lhs.end(), rhs.begin(), rhs.end() ); \
   } \
-  bool operator<=(Collection const& lhs, Collection const& rhs) \
+  inline bool operator<=(Collection const& lhs, Collection const& rhs) \
   { \
     return lhs == rhs || lhs < rhs; \
   } \
-  bool operator>(Collection const& lhs, Collection const& rhs) \
+  inline bool operator>(Collection const& lhs, Collection const& rhs) \
   { \
     return not ( lhs <= rhs ); \
   } \
-  bool operator>=(Collection const& lhs, Collection const& rhs) \
+  inline bool operator>=(Collection const& lhs, Collection const& rhs) \
   { \
     return not ( lhs < rhs ); \
   }
