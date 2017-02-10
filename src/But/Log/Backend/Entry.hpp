93bc92367cffc9b30ff52cd10ab7701331c1e56a
bartek 'basz' szurgot <git@baszerr.eu>
2017-02-10 19:16:24 +0100

    entry elements

---
+++ src/But/Log/Backend/Entry.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <vector>
+#include "FieldInfo.hpp"
+
+namespace But
+{
+namespace Log
+{
+namespace Backend
+{
+
+using Entry = std::vector<FieldInfo>;
+
+}
+}
+}
---
+++ src/But/Log/Backend/FieldInfo.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+
+namespace But
+{
+namespace Log
+{
+namespace Backend
+{
+
+struct FieldInfo final
+{
+  std::string type_;
+  std::string value_;
+};
+
+}
+}
+}
