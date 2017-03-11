9a12e18586caec82ca0d8b60a92b9d5abc2d5d59
bartek 'basz' szurgot <git@baszerr.eu>
2017-03-11 16:37:02 +0100

    brief description of a logger proxy

--- src/But/Log/LoggerProxy.hpp
+++ src/But/Log/LoggerProxy.hpp
@@ -5,6 +5,23 @@ namespace But
 namespace Log
 {
 
+/** @brief proxy class, to be used as an entrypoint for logging.
+ *         Destination type can be anything that derives from Destination::Foregin class,
+ *         or a user-provided type, that will handle all the arguments on its own.
+ *
+ * @example basic, zero-overhead, on-console output logger can be achieved like this:
+ * <code>
+ * using Log = LoggerProxy<Destination::Console>;
+ * Log log;
+ * log.log( Timestamp{}, " hello - current UTC date is: ", UtcDate{} );
+ * </code>
+ *
+ * @example if a dynamic output is needed, use Destination::Foregin as a base class,
+ *          for implementing any output for you want. as a parameter you will receive a
+ *          collection of type-value pairs, reprenseting each argument.
+ *
+ * @warning it is "destination"'s implemented responsability to handile output in a thread-safe manner!
+ */
 template<typename Destination>
 class LoggerProxy final
 {
