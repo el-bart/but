61676dbf262a57538e68774849d409f720c0182b
bartek 'basz' szurgot <git@baszerr.eu>
2017-03-11 23:02:29 +0100

    added example of all priorities

--- src/But/Log/Destination/Syslog.manual.cpp
+++ src/But/Log/Destination/Syslog.manual.cpp
@@ -12,10 +12,20 @@ int main()
   s.log( Timestamp{}, " ", Priority::info, " @ ", FileName{__FILE__}, ":", LineNumber{__LINE__}, " ", "hello, world" );
   s.log( Timestamp{}, " ", Priority::info, " @ ", FileName{__FILE__}, ":", LineNumber{__LINE__}, " ", "bye, world" );
 
+  s.log( Priority::debug, " this is debug");
+  s.log( Priority::info, " this is info");
+  s.log( Priority::warning, " this is warning");
+  s.log( Priority::error, " this is error");
+
   But::Log::Destination::Foregin& base = s;
   base.log( Timestamp{}, " ", Priority::info, " @ ", FileName{__FILE__}, ":", LineNumber{__LINE__}, " ", "hello, foregin world" );
   base.log( Timestamp{}, " ", Priority::info, " @ ", FileName{__FILE__}, ":", LineNumber{__LINE__}, " ", "bye, foregin world" );
 
-  s.log("logging without ", " priority");
-  base.log("logging without ", " priority", ", with base class");
+  base.log( Priority::debug, " this is debug");
+  base.log( Priority::info, " this is info");
+  base.log( Priority::warning, " this is warning");
+  base.log( Priority::error, " this is error");
+
+  s.log("logging without", " priority");
+  base.log("logging without", " priority", ", with base class");
 }
