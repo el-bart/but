32d4d8df898a8fb5d1396ddbea9f6e01ced714e4
bartek 'basz' szurgot <git@baszerr.eu>
2017-03-18 21:02:49 +0100

    Guard class and a helper function added; not fully operation yet

---
+++ src/But/Guard.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <boost/optional.hpp>
+
+namespace But
+{
+
+template<typename F>
+class Guard final
+{
+public:
+  Guard(F&& f): f_{ std::forward<F>(f) } { }
+  ~Guard() noexcept
+  {
+    if(f_)
+      (*f_)();
+  }
+
+  Guard(Guard&&) = default;
+  Guard& operator=(Guard&&) = default;
+
+  Guard(Guard const&) = delete;
+  Guard& operator=(Guard const&) = delete;
+
+  void cancel() { f_.reset(); }
+
+private:
+  boost::optional<F> f_;
+};
+
+
+template<typename F>
+auto makeGuard(F&& f) { return Guard<F>{ std::forward<F>(f) }; }
+
+}
---
+++ src/But/Guard.ut.cpp
@@ -0,0 +1,96 @@
+#include <functional>
+#include <type_traits>
+#include "gtest/gtest.h"
+#include "Guard.hpp"
+
+using But::Guard;
+using But::makeGuard;
+
+namespace
+{
+
+struct ButGuard: public testing::Test
+{ };
+
+
+TEST_F(ButGuard, NonCopyable)
+{
+  using SampleGuard = Guard<void(*)()>;
+  EXPECT_FALSE( std::is_copy_assignable<SampleGuard>::value );
+  EXPECT_FALSE( std::is_copy_constructible<SampleGuard>::value );
+}
+
+unsigned g_triggersCount{0};
+void testTrigger() { ++g_triggersCount; }
+
+TEST_F(ButGuard, CreatingFromFunction)
+{
+  {
+    Guard<void(*)()> g{testTrigger};
+    EXPECT_EQ( 0u, g_triggersCount );
+  }
+  EXPECT_EQ( 1u, g_triggersCount );
+}
+
+
+TEST_F(ButGuard, CreatingFromFunctor)
+{
+  auto count = 0;
+  {
+    Guard<std::function<void()>> g{ [&](){ ++count; } };
+    EXPECT_EQ( 0, count );
+  }
+  EXPECT_EQ( 1, count );
+}
+
+
+TEST_F(ButGuard, Movable)
+{
+  return;                       
+  auto count = 0;
+  {
+    Guard<std::function<void()>> g1{ [&](){ ++count; } };
+    Guard<std::function<void()>> g2{ std::move(g1) };
+    EXPECT_EQ( 0, count );
+  }
+  EXPECT_EQ( 1, count );
+}
+
+
+TEST_F(ButGuard, CancelingPreventsActionExecution)
+{
+  auto count = 0;
+  {
+    Guard<std::function<void()>> g{ [&](){ ++count; } };
+    g.cancel();
+    EXPECT_EQ( 0, count );
+  }
+  EXPECT_EQ( 0, count );
+}
+
+
+TEST_F(ButGuard, CancelingCanHappenMultipleTimes)
+{
+  auto count = 0;
+  {
+    Guard<std::function<void()>> g{ [&](){ ++count; } };
+    g.cancel();
+    g.cancel();
+    g.cancel();
+    EXPECT_EQ( 0, count );
+  }
+  EXPECT_EQ( 0, count );
+}
+
+
+TEST_F(ButGuard, CreatingWithHelperFunction)
+{
+  auto count = 0;
+  {
+    auto g = makeGuard( [&](){ ++count; } );
+    EXPECT_EQ( 0, count );
+  }
+  EXPECT_EQ( 1, count );
+}
+
+}
