7101c9f8a6b090c8b4d5cca86d9696715c5bedcc
bartek 'basz' szurgot <git@baszerr.eu>
2017-03-18 21:34:03 +0100

    Optional<T> implemented and tested; in particular move works as expected :P

---
+++ src/But/Optional.hpp
@@ -0,0 +1,66 @@
+#pragma once
+#include <boost/optional.hpp>
+
+namespace But
+{
+
+/** @brief wrapper for boost::optional, that does not behave well in many releases of boost, namely:
+ *         move-construction does not work, reference types can be accepted, bool() conversion is not explicit.
+ *         these are mostly historical concenrs, but stil valid on older releases at least...
+ */
+template<typename T>
+class Optional
+{
+  static_assert( not std::is_reference<T>::value, "references are not allowed" );
+
+public:
+  using value_type = T;
+
+  Optional() = default;
+  explicit Optional(T&& t): t_{ std::forward<T>(t) } { }
+
+  Optional(Optional const&) = default;
+  Optional& operator=(Optional const&) = default;
+
+  Optional(Optional&& other):
+    t_{ std::move(other.t_) }
+  {
+    other.reset();
+  }
+  Optional& operator=(Optional&& other)
+  {
+    if( this != &other )
+    {
+      t_ = std::move(other.t_);
+      other.reset();
+    }
+    return *this;
+  }
+
+  auto operator->() { return &get(); }
+  auto operator->() const { return &get(); }
+
+  explicit operator bool() const { return static_cast<bool>(t_); }
+
+  T& get()
+  {
+    assert( static_cast<bool>(t_) && "optional is not set" );
+    return t_.get();
+  }
+  T const& get() const
+  {
+    assert( static_cast<bool>(t_) && "optional is not set" );
+    return t_.get();
+  }
+
+  void reset() { t_.reset(); }
+
+private:
+  boost::optional<T> t_;
+};
+
+
+template<typename T, typename ...Args>
+auto makeOptional(Args&& ...args) { return Optional<T>{ std::forward<Args>(args)... }; }
+
+}
---
+++ src/But/Optional.ut.cpp
@@ -0,0 +1,130 @@
+#include <memory>
+#include <string>
+#include <type_traits>
+#include "gtest/gtest.h"
+#include "Optional.hpp"
+
+using But::Optional;
+using But::makeOptional;
+
+namespace
+{
+
+struct ButOptional: public testing::Test
+{ };
+
+
+TEST_F(ButOptional, GettingValue)
+{
+  Optional<std::string> opt{"test"};
+  EXPECT_EQ( "test", opt.get() );
+}
+
+
+TEST_F(ButOptional, ConstCorrectness)
+{
+  const Optional<std::string> opt{"test"};
+  EXPECT_EQ( "test", opt.get() );
+}
+
+
+TEST_F(ButOptional, DefaultConstructor)
+{
+  const Optional<int> opt{};
+  EXPECT_FALSE(opt);
+}
+
+
+TEST_F(ButOptional, Copyable)
+{
+  Optional<std::string> o1{"one"};
+  Optional<std::string> o2 = o1;
+  EXPECT_EQ( "one", o1.get() );
+  EXPECT_EQ( "one", o2.get() );
+}
+
+
+using Ptr = std::unique_ptr<std::string>;
+
+TEST_F(ButOptional, MoveConstructible)
+{
+  Optional<Ptr> p1{ std::make_unique<std::string>("ok") };
+  EXPECT_TRUE(p1);
+  Optional<Ptr> p2{ std::move(p1) };
+  EXPECT_FALSE(p1);
+  ASSERT_TRUE(p2);
+  EXPECT_EQ( "ok", *p2.get() );
+}
+
+
+TEST_F(ButOptional, MoveAssignable)
+{
+  Optional<Ptr> p1{ std::make_unique<std::string>("ok") };
+  EXPECT_TRUE(p1);
+  Optional<Ptr> p2;
+  EXPECT_FALSE(p2);
+
+  p2 = std::move(p1);
+  EXPECT_FALSE(p1);
+  ASSERT_TRUE(p2);
+  EXPECT_EQ( "ok", *p2.get() );
+}
+
+
+TEST_F(ButOptional, SelfMoveAssignmentIsIgnored)
+{
+  Optional<Ptr> p{ std::make_unique<std::string>("ok") };
+  auto& ref = p;
+  p = std::move(ref);
+  EXPECT_TRUE(p);
+  EXPECT_EQ( "ok", *p.get() );
+}
+
+
+TEST_F(ButOptional, Resetable)
+{
+  Optional<std::string> opt{"is set"};
+  ASSERT_TRUE(opt);
+  EXPECT_EQ( "is set", opt.get() );
+  opt.reset();
+  EXPECT_FALSE(opt);
+}
+
+
+TEST_F(ButOptional, ExplicitlyBoolConvertible)
+{
+  const Optional<int> opt{42};
+  EXPECT_TRUE( static_cast<bool>(opt) );
+}
+
+
+TEST_F(ButOptional, ArrowOperator)
+{
+  Optional<std::string> opt{"test"};
+  EXPECT_FALSE( opt->empty() );
+  opt->clear();
+  EXPECT_TRUE( opt->empty() );
+}
+
+
+TEST_F(ButOptional, ConstArrowOperator)
+{
+  const Optional<std::string> opt{"test"};
+  EXPECT_EQ( 4u, opt->size() );
+}
+
+
+TEST_F(ButOptional, HelperMakeFunction)
+{
+  auto opt = makeOptional<std::string>("test");
+  ASSERT_TRUE(opt);
+  EXPECT_EQ( "test", opt.get() );
+}
+
+
+TEST_F(ButOptional, ValueType)
+{
+  EXPECT_TRUE( (std::is_same<std::string, Optional<std::string>::value_type>::value) );
+}
+
+}
