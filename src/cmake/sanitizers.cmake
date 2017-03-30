5576b3a5e708905017fb230e37fa27bcb5b7f40f
bartek 'basz' szurgot <git@baszerr.eu>
2017-03-30 22:07:52 +0200

    sanitizers moved to an upper layer - let user decide if they are to be enabled, or not

--- src/cmake/sanitizers.cmake
+++
@@ -1,5 +0,0 @@
-if(SANITIZE)
-  message(STATUS "enabling ${SANITIZE} sanitizer")
-  add_compile_options(-fsanitize=${SANITIZE})
-  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=${SANITIZE}")
-endif()
