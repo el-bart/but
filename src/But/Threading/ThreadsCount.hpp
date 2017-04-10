025209308c6f7851842700623e26182d2b72132d
bartek 'basz' szurgot <git@baszerr.eu>
2017-04-10 22:20:56 +0200

    class representing threads count

---
+++ src/But/Threading/ThreadsCount.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include "But/Exception.hpp"
+
+namespace But
+{
+namespace Threading
+{
+
+class ThreadsCount final
+{
+public:
+  BUT_DEFINE_EXCEPTION(InvalidNumberOfThreads, Exception, "invalid number of threads");
+
+  using value_type = size_t;
+
+  explicit ThreadsCount(value_type count):
+    count_{count}
+  {
+    if(count_ < 1u)
+      BUT_THROW(InvalidNumberOfThreads, "must be at least 1");
+  }
+
+  value_type count() const { return count_; }
+
+private:
+  value_type count_;
+};
+
+}
+}
---
+++ src/But/Threading/ThreadsCount.ut.cpp
@@ -0,0 +1,25 @@
+#include "gtest/gtest.h"
+#include "ThreadsCount.hpp"
+
+using But::Threading::ThreadsCount;
+
+namespace
+{
+
+struct ButThreadingThreadsCount: public testing::Test
+{ };
+
+
+TEST_F(ButThreadingThreadsCount, PositiveNumberOfThreads)
+{
+  EXPECT_EQ( 1u, ThreadsCount{1}.count() );
+  EXPECT_EQ( 2u, ThreadsCount{2}.count() );
+}
+
+
+TEST_F(ButThreadingThreadsCount, ErrorOnZeroThreads)
+{
+  EXPECT_THROW( ThreadsCount{0}, ThreadsCount::InvalidNumberOfThreads );
+}
+
+}
