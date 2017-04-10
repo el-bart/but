abb4cd49858eb0322370910d47c239ee5590e1a4
bartek 'basz' szurgot <git@baszerr.eu>
2017-04-10 20:09:30 +0200

    ActiveObject is now a header-only class

--- src/But/Threading/ActiveObject.hpp
+++ src/But/Threading/ActiveObject.hpp
@@ -1,8 +1,6 @@
 #pragma once
 #include <memory>
 #include <future>
-#include <thread>
-
 #include "Fifo.hpp"
 #include "JoiningThread.hpp"
 
@@ -14,17 +12,36 @@ namespace Threading
 class ActiveObject final
 {
 public:
-  ActiveObject();
-  ~ActiveObject();
+  ActiveObject():
+    quit_{false},
+    th_{&ActiveObject::processingLoop, this}
+  { }
+
+  ~ActiveObject()
+  {
+    try
+    {
+      quit_ = true;
+      // unblock thread, if needed
+      Queue::lock_type lock{q_};
+      q_.push(Queue::value_type{});
+    }
+    catch(...)
+    {
+      assert(!"failed to stop thread - givin up...");
+      abort();
+    }
+  }
 
   template<typename F>
   auto run(F f)
   {
-    using Ret = decltype(f());
-    auto cmd = std::make_unique<Task<Ret>>( std::move(f) );
-    auto fut = cmd->task_.get_future();
-    Queue::lock_type lock{q_};
-    q_.push( std::move(cmd) );
+    auto cmd = std::make_unique<Task<F>>( std::move(f) );
+    auto fut = cmd->promise_.get_future();
+    {
+      const Queue::lock_type lock{q_};
+      q_.push( std::move(cmd) );
+    }
     return fut;
   }
 
@@ -35,16 +52,66 @@ private:
     virtual void run() = 0;
   };
 
-  template<typename R>
-  struct Task: public Command
+  template<typename F>
+  struct Task final: public Command
   {
-    template<typename F>
-    Task(F f): task_{ std::move(f) } { }
-    virtual void run() noexcept { task_(); }
-    std::packaged_task<R()> task_;
+    explicit Task(F f): f_{ std::move(f) } { }
+
+    virtual void run() noexcept
+    {
+      try
+      {
+        const auto tag = static_cast<value_type const*>(nullptr);
+        runAndSetValue(tag);
+      }
+      catch(...)
+      {
+        promise_.set_exception( std::current_exception() );
+      }
+    }
+
+    void runAndSetValue(void const*)
+    {
+      f_();
+      promise_.set_value();
+    }
+
+    template<typename Ignore>
+    void runAndSetValue(Ignore const*)
+    {
+      promise_.set_value( f_() );
+    }
+
+    F f_;
+    std::promise<decltype(f_())> promise_;
+
+    using value_type = typename std::decay<decltype(f_())>::type;
   };
 
-  void processingLoop() noexcept;
+  auto getCommand()
+  {
+    Queue::lock_type lock{q_};
+    auto cmd = std::move( q_.top(lock) );
+    q_.pop();
+    return cmd;
+  }
+
+  void processingLoop() noexcept
+  {
+    while(not quit_)
+    {
+      try
+      {
+        auto cmd = getCommand();
+        if(cmd)
+          cmd->run();
+      }
+      catch(...)
+      {
+        assert(!"unexpected exception from processing thread - this should never happen");
+      }
+    }
+  }
 
   using Queue = Fifo<std::unique_ptr<Command>>;
 
