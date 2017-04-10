4d26573eb55a7e6515cf741af2274c9bd2de5626
bartek 'basz' szurgot <git@baszerr.eu>
2017-04-10 20:15:06 +0200

    split into ActiveObject and ThreadPool (TP not yet implemented as needed)

---
+++ src/But/Threading/ThreadPool.hpp
@@ -0,0 +1,83 @@
+#pragma once
+#include <memory>
+#include <future>
+#include "Fifo.hpp"
+#include "JoiningThread.hpp"
+#include "detail/Command.hpp"
+
+namespace But
+{
+namespace Threading
+{
+
+class ThreadPool final
+{
+public:
+  ThreadPool():
+    quit_{false},
+    th_{&ThreadPool::processingLoop, this}
+  { }
+
+  ~ThreadPool()
+  {
+    try
+    {
+      quit_ = true;
+      // unblock thread, if needed
+      Queue::lock_type lock{q_};
+      q_.push(Queue::value_type{});
+    }
+    catch(...)
+    {
+      assert(!"failed to stop thread - givin up...");
+      abort();
+    }
+  }
+
+  template<typename F>
+  auto run(F f)
+  {
+    auto cmd = std::make_unique<detail::Task<F>>( std::move(f) );
+    auto fut = cmd->promise_.get_future();
+    {
+      const Queue::lock_type lock{q_};
+      q_.push( std::move(cmd) );
+    }
+    return fut;
+  }
+
+private:
+  auto getCommand()
+  {
+    Queue::lock_type lock{q_};
+    auto cmd = std::move( q_.top(lock) );
+    q_.pop();
+    return cmd;
+  }
+
+  void processingLoop() noexcept
+  {
+    while(not quit_)
+    {
+      try
+      {
+        auto cmd = getCommand();
+        if(cmd)
+          cmd->run();
+      }
+      catch(...)
+      {
+        assert(!"unexpected exception from processing thread - this should never happen");
+      }
+    }
+  }
+
+  using Queue = Fifo<std::unique_ptr<detail::Command>>;
+
+  Queue q_;
+  std::atomic<bool> quit_;
+  JoiningThread<std::thread> th_;
+};
+
+}
+}
---
+++ src/But/Threading/ThreadPool.ut.cpp
@@ -0,0 +1,94 @@
+#include <string>
+#include <chrono>
+
+#include "gtest/gtest.h"
+#include "ThreadPool.hpp"
+
+using ActObj = But::Threading::ThreadPool;
+
+namespace
+{
+
+struct ButThreadingThreadPool: public testing::Test
+{
+  ActObj               ao_;
+  std::chrono::seconds timeout_{5};
+};
+
+
+TEST_F(ButThreadingThreadPool, ClosingRightAway)
+{
+  // leave this empty
+}
+
+
+TEST_F(ButThreadingThreadPool, PassSomethingToProcess)
+{
+  auto f = ao_.run( []{ return std::string{"narf"}; } );
+  ASSERT_TRUE( f.wait_for(timeout_)==std::future_status::ready );
+  EXPECT_EQ( f.get(), "narf" );
+}
+
+
+int getAnswer() { return 42; }
+
+TEST_F(ButThreadingThreadPool, ProcessingFunction)
+{
+  auto f = ao_.run(getAnswer);
+  ASSERT_TRUE( f.wait_for(timeout_)==std::future_status::ready );
+  EXPECT_EQ( f.get(), 42 );
+}
+
+
+struct MovableOnly
+{
+  MovableOnly() = default;
+  MovableOnly(MovableOnly&&) = default;
+  MovableOnly& operator=(MovableOnly&&) = default;
+  int operator()() { return 997; }
+};
+
+TEST_F(ButThreadingThreadPool, ProcessingMovableOnlyFunctor)
+{
+  auto f = ao_.run(MovableOnly{});
+  ASSERT_TRUE( f.wait_for(timeout_)==std::future_status::ready );
+  EXPECT_EQ( f.get(), 997 );
+}
+
+
+struct CustomExceptionType { };
+
+TEST_F(ButThreadingThreadPool, ForwardingExceptions)
+{
+  auto f = ao_.run( []()->int { throw CustomExceptionType{}; } );
+  ASSERT_TRUE( f.wait_for(timeout_)==std::future_status::ready );
+  EXPECT_THROW( f.get(), CustomExceptionType );
+}
+
+
+template<int N>
+int getSth() { return N; }
+
+TEST_F(ButThreadingThreadPool, MultipleCalls)
+{
+  auto f0 = ao_.run(getSth<0>);
+  auto f1 = ao_.run(getSth<1>);
+  auto f2 = ao_.run(getSth<2>);
+  ASSERT_TRUE( f0.wait_for(timeout_)==std::future_status::ready );
+  EXPECT_EQ( f0.get(), 0 );
+  ASSERT_TRUE( f1.wait_for(timeout_)==std::future_status::ready );
+  EXPECT_EQ( f1.get(), 1 );
+  ASSERT_TRUE( f2.wait_for(timeout_)==std::future_status::ready );
+  EXPECT_EQ( f2.get(), 2 );
+}
+
+
+void doNothing() { }
+
+TEST_F(ButThreadingThreadPool, NoReturnValueSmokeTest)
+{
+  auto f = ao_.run(doNothing);
+  ASSERT_TRUE( f.wait_for(timeout_)==std::future_status::ready );
+}
+
+}
