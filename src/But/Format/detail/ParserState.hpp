7a3277ea6ee57e43d688532b6a3bad037559e0c4
bartek 'basz' szurgot <git@baszerr.eu>
2017-04-23 12:48:35 +0200

    class representing parser state

---
+++ src/But/Format/detail/ParserState.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include "But/Container/Array.hpp"
+
+namespace But
+{
+namespace Format
+{
+namespace detail
+{
+
+struct State
+{
+  enum class Type
+  {
+    String,
+    Value,
+    Type
+  };
+
+  char const* begin_{nullptr};
+  char const* end_{nullptr};
+  Type type_{Type::String};
+  unsigned referencedArgument_{0};  // zero means - none.
+};
+
+template<unsigned N>
+struct ParserState final
+{
+  Container::Array<State,N> segments_;
+  unsigned count_{0};
+};
+
+}
+}
+}
