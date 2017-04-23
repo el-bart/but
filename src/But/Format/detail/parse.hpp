c77cf1b9285acdfc1770fce9f6a036c308e60968
bartek 'basz' szurgot <git@baszerr.eu>
2017-04-23 13:28:49 +0200

    strings parsing added

--- src/But/Format/detail/parse.hpp
+++ src/But/Format/detail/parse.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "ParserState.hpp"
+#include "common.hpp"
 
 namespace But
 {
@@ -8,12 +9,60 @@ namespace Format
 namespace detail
 {
 
-template<unsigned N>
-constexpr auto parse(char const* /*fmt*/)
+constexpr auto parseVariable(State& , char const* fmt)
 {
-  ParserState<N> ps;
-  // TODO....
+  return throwOnInvalidSyntax( true, "not yet implemented", fmt );
+}
+
+
+constexpr auto parseStringVar(State& st, char const* fmt)
+{
+  st.type_ = State::Type::String;
+  st.begin_ = fmt;
+  st.end_  = fmt+2;
+  return st.end_;
+}
+
+
+constexpr auto parseString(State& st, char const* fmt)
+{
+  st.type_ = State::Type::String;
+  st.begin_ = fmt;
+  st.end_  = fmt;
+  while( not isEos(*st.end_) && not isVariableBegin(*st.end_) )
+    ++st.end_;
+  return st.end_;
+}
+
+
+template<unsigned N>
+constexpr auto parseImpl(ParserState<N>&& ps, char const* fmt)
+{
+  while( not isEos(*fmt) )
+  {
+    State st;
+    if( isVariableBegin(*fmt) )
+    {
+      throwOnInvalidSyntax( isEos(fmt[1]), "end of data while declaring a variable", fmt+1 );
+      if( isVariableBegin(fmt[1]) ) // "$$" case
+        fmt = parseStringVar(st, fmt);
+      else
+        fmt = parseVariable(st, fmt);
+    }
+    else
+      fmt = parseString(st, fmt);
+
+    ps.segments_[ps.count_] = std::move(st);
+    ++ps.count_;
+  }
   return ps;
+}
+
+
+template<unsigned N>
+constexpr auto parse(char const* fmt)
+{
+  return parseImpl( ParserState<N>{}, fmt );
 }
 
 }
--- src/But/Format/detail/parse.ut.cpp
+++ src/But/Format/detail/parse.ut.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "parse.hpp"
 
+using But::Format::Invalid;
 using But::Format::detail::parse;
 using But::Format::detail::State;
 
@@ -15,11 +16,52 @@ TEST_F(ButFormatDetailParse, UnformattedStrings)
 {
   {
     constexpr auto ps = parse<1>("");
+    ASSERT_EQ( 0u, ps.count_ );
+  }
+  {
+    constexpr auto ps = parse<1>("test string");
     ASSERT_EQ( 1u, ps.count_ );
-    constexpr auto s0 = ps.segments_[0];
-    EXPECT_TRUE( s0.begin_ == s0.end_ );
-    EXPECT_EQ( 0, *s0.begin_ );
-    EXPECT_EQ( State::Type::String, s0.type_ );
+    constexpr auto s = ps.segments_[0];
+    EXPECT_TRUE( s.begin_ != s.end_ );
+    EXPECT_EQ( "test string", std::string(s.begin_, s.end_) );
+    EXPECT_EQ( State::Type::String, s.type_ );
+  }
+  {
+    constexpr auto ps = parse<1>("test\n\r\tstring");
+    ASSERT_EQ( 1u, ps.count_ );
+    constexpr auto s = ps.segments_[0];
+    EXPECT_TRUE( s.begin_ != s.end_ );
+    EXPECT_EQ( "test\n\r\tstring", std::string(s.begin_, s.end_) );
+    EXPECT_EQ( State::Type::String, s.type_ );
+  }
+  {
+    constexpr auto ps = parse<1>("$$");
+    ASSERT_EQ( 1u, ps.count_ );
+    constexpr auto s = ps.segments_[0];
+    EXPECT_EQ( "$$", std::string(s.begin_, s.end_) );
+    EXPECT_EQ( State::Type::String, s.type_ );
+  }
+  {
+    constexpr auto ps = parse<3>("some $$ non-var");
+    ASSERT_EQ( 3u, ps.count_ );
+    // 0
+    {
+      constexpr auto s = ps.segments_[0];
+      EXPECT_EQ( "some ", std::string(s.begin_, s.end_) );
+      EXPECT_EQ( State::Type::String, s.type_ );
+    }
+    // 1
+    {
+      constexpr auto s = ps.segments_[1];
+      EXPECT_EQ( "$$", std::string(s.begin_, s.end_) );
+      EXPECT_EQ( State::Type::String, s.type_ );
+    }
+    // 2
+    {
+      constexpr auto s = ps.segments_[2];
+      EXPECT_EQ( " non-var", std::string(s.begin_, s.end_) );
+      EXPECT_EQ( State::Type::String, s.type_ );
+    }
   }
 }
 
@@ -51,6 +93,14 @@ TEST_F(ButFormatDetailParse, ArgumentsArrayCanBeLargerThanNeeded)
 
 TEST_F(ButFormatDetailParse, MixedTokens)
 {
+}
+
+
+TEST_F(ButFormatDetailParse, InvalidFormats)
+{
+  EXPECT_THROW( parse<10>("$"), Invalid ) << "parameter declaraion too short";
+  EXPECT_THROW( parse<10>("oops $"), Invalid ) << "parameter declaraion too short";
+  EXPECT_THROW( parse<10>("$oops"), Invalid ) << "not a number";
 }
 
 }
