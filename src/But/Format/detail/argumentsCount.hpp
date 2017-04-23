91a90bd040a7bd2c754645ac00a46de8fa8a865e
bartek 'basz' szurgot <git@baszerr.eu>
2017-04-23 12:03:46 +0200

    arguments counting, with a simplified grammar

--- src/But/Format/detail/argumentsCount.hpp
+++ src/But/Format/detail/argumentsCount.hpp
@@ -8,9 +8,81 @@ namespace Format
 namespace detail
 {
 
-constexpr auto argumentsCount(char const* )
+/** @brief this funciton is a workaround for GCC bug... */
+constexpr char const* throwOnInvalidSyntax(const bool error, char const* msg, char const* fmt)
 {
-  return 0u;
+  if(error)
+  {
+    //BUT_THROW(Invalid, "expected either number or opening brace ('{') after variable declaration; got: '" << *fmt << "' instead");
+    throw Invalid{__FILE__, __LINE__, BOOST_CURRENT_FUNCTION, std::string{msg} + "; got '" + std::string{*fmt} + "' instead"};
+    return static_cast<char const*>(nullptr);
+  }
+  return fmt;
+}
+
+constexpr auto isVariableBegin(const char c) { return c == '$'; }
+constexpr auto isEos(const char c) { return c == 0; }
+constexpr auto isDigit(const char c) { return '0' <= c && c <= '9'; }
+constexpr auto isNewLine(const char c) { return c=='\n' || c=='\r'; }
+constexpr auto isSpace(const char c) { return c==' ' || c=='\t' || isNewLine(c); }
+
+constexpr auto consumeUntilTheEndOfString(char const* fmt)
+{
+  while( not isVariableBegin(*fmt) && not isEos(*fmt) )
+    ++fmt;
+  return fmt;
+}
+
+constexpr auto consumeUntilEndOfSimepleVariable(char const* fmt)
+{
+  while( isDigit(*fmt) )
+    ++fmt;
+  if( isEos(*fmt) )
+    return fmt;
+  return throwOnInvalidSyntax( not isSpace(*fmt), "invalid number", fmt );
+}
+
+constexpr auto consumeUntilEndOfBraceVariable(char const* fmt)
+{
+  while( *fmt!='}' && not isEos(*fmt) )
+    ++fmt;
+  return throwOnInvalidSyntax( *fmt!='}', "variable declration not ended with a '}'", fmt );
+}
+
+constexpr auto consumeUntilEndOfVariable(char const* fmt)
+{
+  if( isDigit(*fmt) )
+    return consumeUntilEndOfSimepleVariable(fmt+1);
+  if( *fmt=='{')
+    return consumeUntilEndOfBraceVariable(fmt+1);
+  return throwOnInvalidSyntax(true, "expected either number or opening brace ('{') after variable declaration", fmt);
+}
+
+
+/** @brief helper function that determines number of arguments, based on a simplified syntax grammar.
+ */
+constexpr auto argumentsCount(char const* fmt)
+{
+  auto args = 0u;
+  while(*fmt)
+  {
+    if( isVariableBegin(*fmt) )
+    {
+      ++fmt;
+      if( isEos(*fmt) )
+        throwOnInvalidSyntax(true, "variable declaration terminated in the middle of parsing", fmt);
+      if( isVariableBegin(*fmt) )
+        ++fmt;
+      else
+      {
+        fmt = consumeUntilEndOfVariable(fmt);
+        ++args;
+      }
+    }
+    else
+      fmt = consumeUntilTheEndOfString(fmt+1);
+  }
+  return args;
 }
 
 }
--- src/But/Format/detail/argumentsCount.ut.cpp
+++ src/But/Format/detail/argumentsCount.ut.cpp
@@ -29,10 +29,14 @@ TEST_F(ButFormatDetailArgumentsCount, SimpleStrings)
     constexpr auto cnt = argumentsCount("!@#%^%^*(()");
     EXPECT_EQ( 0u, cnt );
   }
+  {
+    constexpr auto cnt = argumentsCount("test $$ data");
+    EXPECT_EQ( 0u, cnt );
+  }
 }
 
 
-TEST_F(ButFormatDetailArgumentsCount, MacroOneArgument)
+TEST_F(ButFormatDetailArgumentsCount, OneArgument)
 {
   {
     constexpr auto cnt = argumentsCount("$1");
@@ -46,19 +50,69 @@ TEST_F(ButFormatDetailArgumentsCount, MacroOneArgument)
     constexpr auto cnt = argumentsCount("test${1}data");
     EXPECT_EQ( 1u, cnt );
   }
-  EXPECT_THROW( argumentsCount("$1oops"), Invalid );
+  {
+    constexpr auto cnt = argumentsCount("test ${1#ignored text} data");
+    EXPECT_EQ( 1u, cnt );
+  }
+  {
+    constexpr auto cnt = argumentsCount("test ${V1} data");
+    EXPECT_EQ( 1u, cnt );
+  }
+  {
+    constexpr auto cnt = argumentsCount("test ${V1#ignored text} data");
+    EXPECT_EQ( 1u, cnt );
+  }
+  {
+    constexpr auto cnt = argumentsCount("test ${T1} data");
+    EXPECT_EQ( 1u, cnt );
+  }
+  {
+    constexpr auto cnt = argumentsCount("test ${T1#ignored text} data");
+    EXPECT_EQ( 1u, cnt );
+  }
+  {
+    constexpr auto cnt = argumentsCount("test $1\ndata");
+    EXPECT_EQ( 1u, cnt );
+  }
+  {
+    constexpr auto cnt = argumentsCount("test $1\rdata");
+    EXPECT_EQ( 1u, cnt );
+  }
+  {
+    constexpr auto cnt = argumentsCount("test $1\tdata");
+    EXPECT_EQ( 1u, cnt );
+  }
+  {
+    constexpr auto cnt = argumentsCount("test $1 data");
+    EXPECT_EQ( 1u, cnt );
+  }
 }
 
 
-TEST_F(ButFormatDetailArgumentsCount, MacroMultipleArguments)
+TEST_F(ButFormatDetailArgumentsCount, ExceptionIsThrownOnInvalidFormat)
 {
-  // TODO
+  EXPECT_THROW( argumentsCount("$1oops"), Invalid ) << "invalid number";
+  EXPECT_THROW( argumentsCount("${1"), Invalid ) << "missing closing brace";
+  EXPECT_THROW( argumentsCount("${1 x"), Invalid ) << "missing closing brace";
+  EXPECT_THROW( argumentsCount("${1# x"), Invalid ) << "missing closing brace";
+  EXPECT_THROW( argumentsCount("test$"), Invalid ) << "variable declaration too short";
+  EXPECT_THROW( argumentsCount("test $1}"), Invalid ) << "invalid number";
 }
 
 
-TEST_F(ButFormatDetailArgumentsCount, MacroMultipleArgumentsMultipleStyles)
+TEST_F(ButFormatDetailArgumentsCount, MultipleArguments)
 {
-  // TODO
+  {
+    constexpr auto cnt = argumentsCount("test $1 data $3\t$4\n");
+    EXPECT_EQ( 3u, cnt );
+  }
+}
+
+
+TEST_F(ButFormatDetailArgumentsCount, MultipleArgumentsMultipleStyles)
+{
+  constexpr auto cnt = argumentsCount("test $1 data ${3} ${V4#xx} ${T8}");
+  EXPECT_EQ( 4u, cnt );
 }
 
 }
