66a7ad2dc2b0de97c4083f6e72b0cc14e626e76e
bartek 'basz' szurgot <git@baszerr.eu>
2017-06-04 22:00:51 +0200

    fixed optimization issue, that prevented UB sanitizer on GCC from working

--- src/But/detail/testUndefinedBehaviorSanitizer.manual.cpp
+++ src/But/detail/testUndefinedBehaviorSanitizer.manual.cpp
@@ -4,7 +4,7 @@
 int main()
 {
   std::cout << "EXPECTED: you should see an error about overflowing a signed integer" << std::endl;
-  auto overflow = std::numeric_limits<signed>::max();
+  volatile auto overflow = std::numeric_limits<signed>::max();
   ++overflow;
   (void)overflow;
   std::cerr << "NOTE: status code of an application should be non-zero!" << std::endl;
