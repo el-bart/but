ad4633996e027e6f474843d7c33e489bc5a598f8
bartek 'basz' szurgot <git@baszerr.eu>
2017-07-18 21:47:58 +0200

    fixed tests to overcome Clang's heap elision optimization

--- src/But/detail/testLeak.manual.cpp
+++ src/But/detail/testLeak.manual.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <cstring>
 
 int main()
 {
   std::cout << "EXPECTED: you should see an error about memory leak" << std::endl;
   auto leak = new char[42];
-  (void)leak;
+  memcpy(leak, "oops", 5);
+  std::cout << leak << std::endl;
   std::cerr << "NOTE: status code of an application should be non-zero!" << std::endl;
 }
