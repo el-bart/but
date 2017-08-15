a8c1af72bac68a25cb40989dca644874e48fec85
bartek 'basz' szurgot <git@baszerr.eu>
2017-08-15 20:49:32 +0200

    implementation of argumentsCount() now correctly counts repeated arguments

--- src/But/Format/detail/argumentsCount.hpp
+++ src/But/Format/detail/argumentsCount.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "But/Container/Array.hpp"
 #include "lastArgumentNumber.hpp"
 
 namespace But
@@ -9,17 +10,26 @@ namespace detail
 {
 
 template<unsigned N>
+constexpr auto bitsCount(Container::Array<bool,N> const& tab)
+{
+  auto count = 0u;
+  for(auto b: tab)
+    count += b;
+  return count;
+}
+
+template<unsigned N>
 constexpr auto argumentsCount(ParserState<N> const& ps)
 {
-  // TODO: repeated arguments should not be taken into account!
-  auto count = 0u;
+  Container::Array<bool,N> tab{};
+  tab.fill(false);
   for(auto i=0u; i<ps.count_; ++i)
   {
     auto& e = ps.segments_[i];
     if( e.type_ == State::Type::Value || e.type_ == State::Type::TypeName )
-      ++count;
+      tab[e.referencedArgument_] = true;
   }
-  return count;
+  return bitsCount(tab);
 }
 
 }
