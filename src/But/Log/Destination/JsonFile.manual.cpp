87035d7f66946606a7f44d9722223eec8adf406d
bartek 'basz' szurgot <git@baszerr.eu>
2017-08-16 22:58:16 +0200

    Json file writer manual test

---
+++ src/But/Log/Destination/JsonFile.manual.cpp
@@ -0,0 +1,37 @@
+#include "JsonFile.hpp"
+#include "But/Log/Field/Timestamp.hpp"
+#include "But/Log/Field/LineNumber.hpp"
+#include "But/Log/Field/Priority.hpp"
+#include "But/Log/Field/FileName.hpp"
+
+using namespace But::Log::Field;
+
+int main()
+{
+  But::Log::Destination::JsonFile jf{"log.json"};
+  jf.log( Timestamp{}, " ", Priority::info, " @ ", FileName{__FILE__}, ":", LineNumber{__LINE__}, " ", "hello, world" );
+  jf.log( Timestamp{}, " ", Priority::info, " @ ", FileName{__FILE__}, ":", LineNumber{__LINE__}, " ", "bye, world" );
+
+  jf.log( Priority::debug, " this is debug");
+  jf.log( Priority::info, " this is info");
+  jf.log( Priority::warning, " this is warning");
+  jf.log( Priority::error, " this is error");
+
+  But::Log::Destination::Foreign& base = jf;
+  base.log( Timestamp{}, " ", Priority::info, " @ ", FileName{__FILE__}, ":", LineNumber{__LINE__}, " ", "hello, foreign world" );
+  base.log( Timestamp{}, " ", Priority::info, " @ ", FileName{__FILE__}, ":", LineNumber{__LINE__}, " ", "bye, foreign world" );
+
+  base.log( Priority::debug, " this is debug");
+  base.log( Priority::info, " this is info");
+  base.log( Priority::warning, " this is warning");
+  base.log( Priority::error, " this is error");
+
+  jf.log("logging without", " priority");
+  base.log("logging without", " priority", ", with base class");
+
+  jf.flush();
+  base.flush();
+
+  jf.reload();
+  base.reload();
+}
