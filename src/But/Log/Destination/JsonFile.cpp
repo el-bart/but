da52452f275c5b36129b2a60854b504f68606923
bartek 'basz' szurgot <git@baszerr.eu>
2017-08-26 22:00:44 +0200

    removed most of the cases for different types

--- src/But/Log/Destination/JsonFile.cpp
+++ src/But/Log/Destination/JsonFile.cpp
@@ -23,65 +23,22 @@ auto createField(Backend::FieldInfo const& fi)
 {
   json field;
 
-  // TODO: optimize this...
-
-  if( fi.type() == "short" )
-  {
-    field[ std::move(fi).type() ] = convert<short>( fi.value() );
-    return field;
-  }
-  if( fi.type() == "unsigned short" )
-  {
-    field[ std::move(fi).type() ] = convert<unsigned short>( fi.value() );
-    return field;
-  }
+  // TODO: how about keeping value as a std::variant, instead of a string?
 
   if( fi.type() == "int" )
   {
-    field[ std::move(fi).type() ] = convert<int>( fi.value() );
+    field[ std::move(fi).type() ] = convert<int64_t>( fi.value() );
     return field;
   }
   if( fi.type() == "unsigned int" )
   {
-    field[ std::move(fi).type() ] = convert<unsigned int>( fi.value() );
+    field[ std::move(fi).type() ] = convert<uint64_t>( fi.value() );
     return field;
   }
 
-  if( fi.type() == "long" )
-  {
-    field[ std::move(fi).type() ] = convert<long>( fi.value() );
-    return field;
-  }
-  if( fi.type() == "unsigned long" )
-  {
-    field[ std::move(fi).type() ] = convert<unsigned long>( fi.value() );
-    return field;
-  }
-
-  if( fi.type() == "long long" )
-  {
-    field[ std::move(fi).type() ] = convert<long long>( fi.value() );
-    return field;
-  }
-  if( fi.type() == "unsigned long long" )
-  {
-    field[ std::move(fi).type() ] = convert<unsigned long long>( fi.value() );
-    return field;
-  }
-
-  if( fi.type() == "float" )
-  {
-    field[ std::move(fi).type() ] = convert<float>( fi.value() );
-    return field;
-  }
   if( fi.type() == "double" )
   {
     field[ std::move(fi).type() ] = convert<double>( fi.value() );
-    return field;
-  }
-  if( fi.type() == "long double" )
-  {
-    field[ std::move(fi).type() ] = convert<long double>( fi.value() );
     return field;
   }
 
