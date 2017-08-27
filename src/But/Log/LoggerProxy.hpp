5bf21749a8ebf5701984d3dc15b1ce06bfdb0a40
bartek 'basz' szurgot <git@baszerr.eu>
2017-08-27 16:06:33 +0200

    documentation made more verbose

--- src/But/Log/LoggerProxy.hpp
+++ src/But/Log/LoggerProxy.hpp
@@ -7,27 +7,48 @@ namespace But
 namespace Log
 {
 
-/** @brief proxy class, to be used as an entrypoint for logging.
+/** @brief proxy class, to be used as an entry point for logging.
  *         Destination type can be anything that derives from Destination::Foreign class,
  *         or a user-provided type, that will handle all the arguments on its own.
  *
- * @example basic, zero-overhead, on-console output logger can be achieved like this:
+ * @warning LoggerProxy is NOT thread-safe! "destinations" however are. what it means is one can re-use
+ *          shared destinations between threads, but each thread shall have own copy of LoggerProxy object to use.
+ *
+ * @example basic, on-console output logger can be achieved like this:
  * <code>
  * using Log = LoggerProxy<Destination::Console>;
  * Log log;
  * log.log( Timestamp{}, " hello - current UTC date is: ", UtcDate{} );
  * </code>
  *
+ * @example the most trivial, near-to-zero overhead, on-console output logger can be achieved like this:
+ * <code>
+ * using Log = LoggerProxy<Destination::NaiveConsole>;
+ * Log log;
+ * log.log( Timestamp{}, " hello - current UTC date is: ", UtcDate{} );
+ * </code>
+ *
+ * @example multi-thread safe logging to file, with all attributes preserved, one JSON per line:
+ * <code>
+ * using Thread = But::JoiningThread<std::thread>;
+ * using Log = LoggerProxy<But::NotNullShared<Destination::Foreign>;
+ * Log log{ But::makeSharedNN<Destination::JsonFile>("/tmp/my_program.log") };
+ * // note: each thread gets its own COPY of LoggerProxy object!
+ * auto action = [log]() { log.log( Timestamp{}, " hello - current UTC date is: ", UtcDate{} ); };
+ * Thread th1{action};
+ * Thread th2{action};
+ * </code>
+ *
  * @example if a dynamic output is needed, use Destination::Foreign as a base class,
  *          for implementing any output for you want. as a parameter you will receive a
- *          collection of type-value pairs, reprenseting each argument.
+ *          collection of type-value pairs, representing each argument.
  *
  * @note this class offers an option to either use your Destination directly (eg. Destination::Console),
- *       so that you can configure logger with a sinle typedef, or provide a runtime-dependent
+ *       so that you can configure logger with a single typedef, or provide a runtime-dependent
  *       argument (i.e. Destination::Foreign derived class) and allow to change destination, depending
  *       on eg. config file entries.
  *
- * @warning it is "destination"'s implementer responsability to handile input/output in a thread-safe manner!
+ * @warning it is "destination"'s implementer responsibility to handle input/output in a thread-safe manner!
  *
  * @note all Destinations must either be (smart) pointers or provide an arrow operator.
  */
@@ -65,7 +86,7 @@ public:
   }
 
   /** @brief triggers destination-specific reload actions (re-open output file, reconnect
-   *         network socket, etc...). typicaly used for implementing easy log rotation.
+   *         network socket, etc...). typically used for implementing easy log rotation.
    */
   void reload()
   {
