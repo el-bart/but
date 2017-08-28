efc75b42ef396e0e0cfd7e134ffef9ecf05ff8bc
bartek 'basz' szurgot <git@baszerr.eu>
2017-08-28 20:57:55 +0200

    added smoke-test for long and dense formats

--- src/But/Format/format.ut.cpp
+++ src/But/Format/format.ut.cpp
@@ -82,6 +82,48 @@ TEST_F(ButFormat, DenseFormats)
     EXPECT_EQ( 0u, fmt.expectedArguments() );
     EXPECT_EQ( "$x", fmt.format() );
   }
+  {
+    constexpr auto fmt = BUT_FORMAT("$0 $0");
+    EXPECT_EQ( 1u, fmt.expectedArguments() );
+    EXPECT_EQ( "1 1", fmt.format(1) );
+  }
+  {
+    constexpr auto fmt = BUT_FORMAT("$0 $0 $0");
+    EXPECT_EQ( 1u, fmt.expectedArguments() );
+    EXPECT_EQ( "1 1 1", fmt.format(1) );
+  }
+  {
+    constexpr auto fmt = BUT_FORMAT("$0 $0 $0 $0");
+    EXPECT_EQ( 1u, fmt.expectedArguments() );
+    EXPECT_EQ( "1 1 1 1", fmt.format(1) );
+  }
+  {
+    constexpr auto fmt = BUT_FORMAT("$0 $0 $0 $0 $0");
+    EXPECT_EQ( 1u, fmt.expectedArguments() );
+    EXPECT_EQ( "1 1 1 1 1", fmt.format(1) );
+  }
+  {
+    constexpr auto fmt = BUT_FORMAT("$0 $0 $0 $0 $0 $0");
+    EXPECT_EQ( 1u, fmt.expectedArguments() );
+    EXPECT_EQ( "1 1 1 1 1 1", fmt.format(1) );
+  }
+  {
+    constexpr auto fmt = BUT_FORMAT("$0 $0 $0 $0 $0 $0 $0");
+    EXPECT_EQ( 1u, fmt.expectedArguments() );
+    EXPECT_EQ( "1 1 1 1 1 1 1", fmt.format(1) );
+  }
+  {
+    constexpr auto fmt = BUT_FORMAT("$0 $0 $0 $0 $0 $0 $0 $0");
+    EXPECT_EQ( 1u, fmt.expectedArguments() );
+    EXPECT_EQ( "1 1 1 1 1 1 1 1", fmt.format(1) );
+  }
+  /*
+  {
+    constexpr auto fmt = BUT_FORMAT("$2 $1 $0 $0 $0");
+    EXPECT_EQ( 3u, fmt.expectedArguments() );
+    EXPECT_EQ( "3 2 1 1 1", fmt.format(1,2,3) );
+  }
+  */
 }
 
 
