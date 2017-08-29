b48ae8c3da76e4103eb6a68e8e507fc322ff05a2
bartek 'basz' szurgot <git@baszerr.eu>
2017-08-29 22:14:09 +0200

    added TODO to check if it is possible to detect issues in release mode, in constexpr contexts only

--- src/But/ConstexprAssert.hpp
+++ src/But/ConstexprAssert.hpp
@@ -5,6 +5,7 @@ namespace But
 {
 
 #ifdef NDEBUG
+// TODO: is it possible to enable constexpr assert only in constexpr context, in non-debug mode?
 #define BUT_CONSTEXPR_ASSERT(cond) do { } while(false)
 #else
 #define BUT_CONSTEXPR_ASSERT(cond) do { if(not (cond)) assert(!#cond); } while(false)
