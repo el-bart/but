e2b4f0b1fb754765ab831bcaa9e00e8b84e64dfc
bartek 'basz' szurgot <git@baszerr.eu>
2017-09-21 20:30:01 +0200

    moved json-related tests to a separate file

---
+++ src/But/Log/Destination/Common/toJsonStream.ut.cpp
@@ -0,0 +1,98 @@
+#include <vector>
+#include <sstream>
+#include "gtest/gtest.h"
+#include "toJsonStream.hpp"
+
+using But::Log::Backend::Entry;
+using But::Log::Destination::Common::toJsonStream;
+
+namespace
+{
+
+struct ButLogDestinationCommonToJsonSteam: public testing::Test
+{
+  void addEntry(Entry&) const { }
+
+  template<typename Head, typename ...Tail>
+  void addEntry(Entry& e, Head const& head, Tail const& ...tail) const
+  {
+    e.emplace_back(head);
+    addEntry(e, tail...);
+  }
+
+  template<typename ...Args>
+  auto toEntry(const Args... args) const
+  {
+    Entry e;
+    addEntry(e, args...);
+    return e;
+  }
+
+
+  template<typename T>
+  auto testType(std::string const& typeName, const T t, std::string const& expectedValue) const
+  {
+    std::stringstream ss;
+    toJsonStream( ss, toEntry(t) );
+    const auto expected = "[{\"" + typeName + "\":" + expectedValue + "}]";
+    const auto read = ss.str();
+    EXPECT_EQ(expected, read);
+    return expected==read;
+  }
+
+  template<typename T>
+  auto testType(std::string const& typeName, const T t) const
+  {
+    return testType( typeName, t, std::to_string(t) );
+  }
+
+  std::stringstream ss_;
+};
+
+
+TEST_F(ButLogDestinationCommonToJsonSteam, EscapingNonprintableCharacters)
+{
+  toJsonStream( ss_, toEntry("A\nB\rC") );
+  EXPECT_EQ( "[{\"string\":\"A\\nB\\rC\"}]", ss_.str() );
+}
+
+
+TEST_F(ButLogDestinationCommonToJsonSteam, SomeLogs)
+{
+  toJsonStream( ss_, toEntry("answer", 42) );
+  toJsonStream( ss_, toEntry("foo", "bar") );
+
+  EXPECT_EQ( R"xx([{"string":"answer"},{"int":42}])xx"
+             R"xx([{"string":"foo"},{"string":"bar"}])xx"
+             , ss_.str() );
+}
+
+
+TEST_F(ButLogDestinationCommonToJsonSteam, SomeFormattedLog)
+{
+  toJsonStream( ss_, toEntry( But::Log::Field::FormattedString{"kszy"}, "answer", 42 ) );
+  EXPECT_EQ( R"xx([{"But::Formatted":"kszy"},{"string":"answer"},{"int":42}])xx",
+             ss_.str() );
+}
+
+
+TEST_F(ButLogDestinationCommonToJsonSteam, NumericTypes)
+{
+  EXPECT_TRUE( testType<short>("int", -42) );
+  EXPECT_TRUE( testType<unsigned short>("unsigned int", 42) );
+
+  EXPECT_TRUE( testType<int>("int", -42) );
+  EXPECT_TRUE( testType<unsigned int>("unsigned int", 42) );
+
+  EXPECT_TRUE( testType<long>("int", -42) );
+  EXPECT_TRUE( testType<unsigned long>("unsigned int", 42) );
+
+  EXPECT_TRUE( testType<long long>("int", -42) );
+  EXPECT_TRUE( testType<unsigned long long>("unsigned int", 42) );
+
+  EXPECT_TRUE( testType<float>("double", -4.0, "-4") );
+  EXPECT_TRUE( testType<double>("double", -4.2, "-4.2") );
+  EXPECT_TRUE( testType<long double>("double", -4.2, "-4.2") );
+}
+
+}
--- src/But/Log/Destination/JsonFile.it.cpp
+++ src/But/Log/Destination/JsonFile.it.cpp
@@ -47,37 +47,6 @@ struct ButLogDestinationJsonFile: public testing::Test
     return ss.str();
   }
 
-  auto readLastLogLine()
-  {
-    tf_.flush();
-    std::ifstream is{ tmp_.path().string() };
-    if( not is.is_open() )
-      throw std::runtime_error{"cannot open file: " + tmp_.path().string() };
-    std::vector<std::string> lines;
-    while(is)
-    {
-      std::string line;
-      std::getline(is, line);
-      lines.push_back( std::move(line) );
-    }
-    return lines.at( lines.size() - 2u );
-  }
-
-  template<typename T>
-  auto testType(std::string const& typeName, const T t, std::string const& expectedValue)
-  {
-    tf_.log(t);
-    const auto expected = "[{\"" + typeName + "\":" + expectedValue + "}]";
-    const auto read = readLastLogLine();
-    EXPECT_EQ(expected, read);
-    return expected==read;
-  }
-  template<typename T>
-  auto testType(std::string const& typeName, const T t)
-  {
-    return testType( typeName, t, std::to_string(t) );
-  }
-
   But::System::TempFile tmp_{"/tmp/but_text_file_destination_it"};
   JsonFile tf_{ tmp_.path() };
 };
@@ -93,16 +62,6 @@ TEST_F(ButLogDestinationJsonFile, LinesAreBeingWritten)
   tf_.log("the answer is: ", 42);
   EXPECT_EQ( 2u, countLines() );
 }
-
-
-TEST_F(ButLogDestinationJsonFile, UsingNonprintableCharacters)
-{
-  tf_.log("A\nB\rC");
-  EXPECT_EQ( 1u, countLines() );
-
-  EXPECT_EQ( "[{\"string\":\"A\\nB\\rC\"}]\n", readLogFile() );
-}
-
 
 TEST_F(ButLogDestinationJsonFile, SomeLogs)
 {
@@ -126,26 +85,6 @@ TEST_F(ButLogDestinationJsonFile, SomeFormattedLog)
   EXPECT_EQ( R"xx([{"But::Formatted":"kszy"},{"string":"answer"},{"int":42}])xx"
              "\n"
              , readLogFile() );
-}
-
-
-TEST_F(ButLogDestinationJsonFile, NumericTypes)
-{
-  EXPECT_TRUE( testType<short>("int", -42) );
-  EXPECT_TRUE( testType<unsigned short>("unsigned int", 42) );
-
-  EXPECT_TRUE( testType<int>("int", -42) );
-  EXPECT_TRUE( testType<unsigned int>("unsigned int", 42) );
-
-  EXPECT_TRUE( testType<long>("int", -42) );
-  EXPECT_TRUE( testType<unsigned long>("unsigned int", 42) );
-
-  EXPECT_TRUE( testType<long long>("int", -42) );
-  EXPECT_TRUE( testType<unsigned long long>("unsigned int", 42) );
-
-  EXPECT_TRUE( testType<float>("double", -4.0, "-4") );
-  EXPECT_TRUE( testType<double>("double", -4.2, "-4.2") );
-  EXPECT_TRUE( testType<long double>("double", -4.2, "-4.2") );
 }
 
 }
