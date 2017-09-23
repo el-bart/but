f6113c1706c9b843544b3d1c8889007003ff6c52
bartek 'basz' szurgot <git@baszerr.eu>
2017-09-23 21:52:06 +0200

    fixed FormattedString field toValue() function

--- src/But/Log/Field/FormattedString.hpp
+++ src/But/Log/Field/FormattedString.hpp
@@ -15,7 +15,7 @@ struct FormattedString final
 };
 
 inline std::string toType(FormattedString const&) { return "But::Formatted"; }
-inline Backend::Value toValue(FormattedString fs) { return std::move(fs.value_); }
+inline Backend::Value toValue(FormattedString fs) { return Backend::Value{ std::move(fs.value_) }; }
 
 inline auto operator==(FormattedString const& lhs, FormattedString const& rhs) { return lhs.value_ == rhs.value_; }
 
