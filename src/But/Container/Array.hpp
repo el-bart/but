b4b0c456b1a72ed9a64cb988e21efb11d8ca419f
bartek 'basz' szurgot <git@baszerr.eu>
2017-09-25 23:15:21 +0200

    using size_type for indexing

--- src/But/Container/Array.hpp
+++ src/But/Container/Array.hpp
@@ -9,7 +9,7 @@ namespace Container
 /** @brief std::array<> replacement, until C++17 arrives with constexpr std::array<> implementation.
  *  @note implementation is very simplistic, as it is not expected to leave long... ;)
  */
-template<typename T, unsigned N>
+template<typename T, size_t N>
 struct Array
 {
   using difference_type = std::ptrdiff_t;
@@ -18,11 +18,11 @@ struct Array
   using iterator = T*;
   using const_iterator = T const*;
 
-  constexpr auto const& operator[](unsigned i) const { return value_[i]; }
-  constexpr auto& operator[](unsigned i) { return value_[i]; }
+  constexpr auto const& operator[](size_type i) const { return value_[i]; }
+  constexpr auto& operator[](size_type i) { return value_[i]; }
 
   constexpr size_type size() const { return N; }
-  constexpr size_t max_size() const { return N; }
+  constexpr size_type max_size() const { return N; }
 
   constexpr auto begin() const { return value_; }
   constexpr auto end() const { return begin() + N; }
@@ -40,19 +40,19 @@ struct Array
 };
 
 
-template<typename T, unsigned N>
+template<typename T, size_t N>
 constexpr auto begin(Array<T,N>& tab) { return tab.begin(); }
-template<typename T, unsigned N>
+template<typename T, size_t N>
 constexpr auto end(Array<T,N>& tab) { return tab.end(); }
 
-template<typename T, unsigned N>
+template<typename T, size_t N>
 constexpr auto begin(Array<T,N> const& tab) { return tab.begin(); }
-template<typename T, unsigned N>
+template<typename T, size_t N>
 constexpr auto end(Array<T,N> const& tab) { return tab.end(); }
 
-template<typename T, unsigned N>
+template<typename T, size_t N>
 constexpr auto cbegin(Array<T,N> const& tab) { return tab.begin(); }
-template<typename T, unsigned N>
+template<typename T, size_t N>
 constexpr auto cend(Array<T,N> const& tab) { return tab.end(); }
 
 }
