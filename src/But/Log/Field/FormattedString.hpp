071c45083c728e11a0bd544bf13f950877d960b6
bartek 'basz' szurgot <git@baszerr.eu>
2017-10-05 22:54:53 +0200

    moved to use toFieldInfo()

--- src/But/Log/Field/FormattedString.hpp
+++ src/But/Log/Field/FormattedString.hpp
@@ -1,7 +1,6 @@
 #pragma once
 #include <string>
-#include "But/Log/Backend/Type.hpp"
-#include "But/Log/Backend/Value.hpp"
+#include "But/Log/Backend/FieldInfo.hpp"
 
 namespace But
 {
@@ -15,8 +14,10 @@ struct FormattedString final
   std::string value_;
 };
 
-inline auto toType(FormattedString const&) { return Backend::Type{"But::Formatted"}; }
-inline auto toValue(FormattedString fs) { return Backend::Value{ std::move(fs.value_) }; }
+inline auto toFieldInfo(FormattedString fs)
+{
+  return Backend::FieldInfo{ Backend::Type{"But::Formatted"}, Backend::Value{ std::move(fs.value_) } };
+}
 
 inline auto operator==(FormattedString const& lhs, FormattedString const& rhs) { return lhs.value_ == rhs.value_; }
 
