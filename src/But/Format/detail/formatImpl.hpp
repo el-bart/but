8606d1f875d37172c6a51a07bb98ab6d957a9abd
bartek 'basz' szurgot <git@baszerr.eu>
2017-10-09 21:29:53 +0200

    interface aligned to new Parsed* classes

--- src/But/Format/detail/formatImpl.hpp
+++ src/But/Format/detail/formatImpl.hpp
@@ -9,17 +9,11 @@ namespace Format
 namespace detail
 {
 
-template<size_t ArgumentsCount>
-struct CompiletimeCheckedFormatProxy final
-{
-  static auto make(char const* fmt) { return Parsed{fmt}; }
-};
-
 #define BUT_FORMAT_DETAIL_FORMAT_COMPILETIME_IMPL(fmt, N) \
-    ::But::Format::detail::CompiletimeChckedFormatProxy< ::But::Format::detail::argumentsCountWithChecks<N>(fmt) >::make(fmt)
+    ::But::Format::ParsedCompiletime< ::But::Format::detail::argumentsCountWithChecks<N>(fmt), N >::make(fmt)
 
 #define BUT_FORMAT_DETAIL_FORMAT_RUNTIME_IMPL(fmt) \
-    ::But::Format::Parsed{fmt}
+    ::But::Format::ParsedRuntime{fmt}
 
 }
 }
