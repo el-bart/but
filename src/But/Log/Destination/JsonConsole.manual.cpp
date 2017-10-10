c9007e3f38d20171e67021d442127a0dee89ba69
bartek 'basz' szurgot <git@baszerr.eu>
2017-10-11 00:12:22 +0200

    added manual test for logging nested data structures with json

--- src/But/Log/Destination/JsonConsole.manual.cpp
+++ src/But/Log/Destination/JsonConsole.manual.cpp
@@ -1,10 +1,39 @@
 #include "JsonConsole.hpp"
+#include "But/Log/Proxy.hpp"
 #include "But/Log/Field/Timestamp.hpp"
 #include "But/Log/Field/LineNumber.hpp"
 #include "But/Log/Field/Priority.hpp"
 #include "But/Log/Field/FileName.hpp"
 
 using namespace But::Log::Field;
+using But::Log::Backend::Tag;
+using But::Log::Backend::FieldInfo;
+
+
+struct Point
+{
+  int x_;
+  int y_;
+};
+
+auto toFieldInfo(Point const& p)
+{
+  using But::Log::Backend::toFieldInfo;
+  return FieldInfo{ Tag{"Point"}, { toFieldInfo(p.x_).retag(Tag{"x"}), toFieldInfo(p.y_).retag(Tag{"y"}) } };
+}
+
+struct Line
+{
+  Point from_;
+  Point to_;
+};
+
+auto toFieldInfo(Line const& l)
+{
+  using But::Log::Backend::toFieldInfo;
+  return FieldInfo{ Tag{"Line"}, { toFieldInfo(l.from_).retag(Tag{"from"}), toFieldInfo(l.to_).retag(Tag{"to"}) } };
+}
+
 
 int main()
 {
@@ -20,4 +49,11 @@ int main()
   base.log( Timestamp{}, Priority::info, '@', FileName{__FILE__}, ':', LineNumber{__LINE__}, "bye, foreign world" );
   base.flush();
   base.reload();
+
+  But::Log::Proxy<> lp{ But::makeSharedNN<But::Log::Destination::JsonConsole>() };
+  lp.log( Timestamp{}, Priority::info, '@', FileName{__FILE__}, ':', LineNumber{__LINE__}, "hello, world" );
+  lp.log( Timestamp{}, Priority::info, '@', FileName{__FILE__}, ':', LineNumber{__LINE__}, "bye, world" );
+  lp.log( BUT_FORMAT("format test - $0"), "whatever..." );
+  lp.log( "simple structure", Point{1,2} );
+  lp.log( "nested structure", Line{ Point{3,4}, Point{9,8} } );
 }
