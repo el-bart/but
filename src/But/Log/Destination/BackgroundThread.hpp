eee4de68a32c0a671ab45b6637a7493ccfd5fdd4
bartek 'basz' szurgot <git@baszerr.eu>
2017-10-16 23:13:17 +0200

    BackgroundThread API draft

---
+++ src/But/Log/Destination/BackgroundThread.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include "Sink.hpp"
+#include "But/Thrading/Fifo.hpp"
+
+namespace But
+{
+namespace Log
+{
+namespace Destination
+{
+
+/** @brief proxy-sink, that accepts all logs and forwards them to a separate thread.
+ *  @warning this proxy does not forward errors from the background thread - they are ignored, instead.
+ */
+class BackgroundThread final: public Sink
+{
+public:
+  BackgroundThread(But::NotNullShared<Sink> chained);
+  ~BackgroundThread();
+
+private:
+  void logImpl(Backend::FieldInfo const& fi);
+  void reloadImpl() override;
+  void flushImpl() override;
+
+  But::NotNullShared<Sink> chained_;
+};
+
+}
+}
+}
