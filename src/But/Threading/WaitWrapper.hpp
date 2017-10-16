1fc211f630a15e9c979898bdf7efe274aed90a41
bartek 'basz' szurgot <git@baszerr.eu>
2017-10-16 21:37:13 +0200

    WaitWrapper no longer throws on timeout

--- src/But/Threading/WaitWrapper.hpp
+++ src/But/Threading/WaitWrapper.hpp
@@ -1,36 +1,34 @@
 #pragma once
 #include <chrono>
 #include <utility>
-#include "But/Exception.hpp"
-
 
 namespace But
 {
 namespace Threading
 {
 
-template<typename TimeoutException, typename ConditionVariable, typename Lock>
+template<typename ConditionVariable, typename Lock>
 struct WaitWrapper final
 {
+  WaitWrapper() = delete;
+
   template<typename Pred>
-  static void wait(ConditionVariable& cv, Lock& lock, Pred p)
+  static bool wait(ConditionVariable& cv, Lock& lock, Pred p)
   {
     cv.wait(lock, std::move(p));
+    return true;
   }
 
   template<typename R, typename P, typename Pred>
-  static void wait(ConditionVariable& cv, Lock& lock, Pred p, const std::chrono::duration<R,P>& timeout)
+  static bool wait(ConditionVariable& cv, Lock& lock, Pred&& p, const std::chrono::duration<R,P>& timeout)
   {
-    if( not cv.wait_for(lock, timeout, std::move(p)) )
-      BUT_THROW(TimeoutException, std::chrono::duration_cast<std::chrono::milliseconds>(timeout).count() << "[ms] passed");
+    return cv.wait_for( lock, timeout, std::forward<Pred>(p) );
   }
 
   template<typename C, typename D, typename Pred>
-  static void wait(ConditionVariable& cv, Lock& lock, Pred p, const std::chrono::time_point<C,D>& deadline)
+  static auto wait(ConditionVariable& cv, Lock& lock, Pred&& p, const std::chrono::time_point<C,D>& deadline)
   {
-    const auto timeout = deadline - C::now();
-    if( not cv.wait_until(lock, deadline, std::move(p)) )
-      BUT_THROW(TimeoutException,  std::chrono::duration_cast<std::chrono::milliseconds>(timeout).count() << "[ms] passed");
+    return cv.wait_until( lock, deadline, std::forward<Pred>(p) );
   }
 };
 
