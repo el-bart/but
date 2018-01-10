7b43ab95a4c6995fb1f96ac3062fdeed8561749a
bartek 'basz' szurgot <git@baszerr.eu>
2018-01-10 22:12:57 +0100

    removed tests covered by LockProxy

--- src/But/Threading/LockProxyProvider.ut.cpp
+++ src/But/Threading/LockProxyProvider.ut.cpp
@@ -26,18 +26,6 @@ struct ButThreadingLockProxyProvider: public testing::Test
 };
 
 
-TEST_F(ButThreadingLockProxyProvider, ProxyLocksAndUnlocksObjectAutomatically)
-{
-  EXPECT_EQ( md_.locks_, 0 );
-  {
-    auto p = md_.withLock();
-    (void)p;
-    EXPECT_EQ( md_.locks_, 1 );
-  }
-  EXPECT_EQ( md_.locks_, 0 );
-}
-
-
 TEST_F(ButThreadingLockProxyProvider, ArrowOperatorOnProxyForwardsToActualClass)
 {
   EXPECT_EQ(md_.locks_, 0);
@@ -53,70 +41,6 @@ TEST_F(ButThreadingLockProxyProvider, ArrowOperatorEnsureConstness)
 
   const auto& cmd = md_;
   cmd.withLock()->constFoo();
-}
-
-
-TEST_F(ButThreadingLockProxyProvider, ProxyIsMovable)
-{
-  EXPECT_TRUE( std::is_move_constructible<LockProxy<MyData>>::value );
-  EXPECT_TRUE( std::is_move_assignable<LockProxy<MyData>>::value );
-}
-
-
-TEST_F(ButThreadingLockProxyProvider, MovingProxyDoesNotCauseExtraLockingNorUnlocking)
-{
-  EXPECT_TRUE( std::is_move_constructible<LockProxy<MyData>>::value );
-  EXPECT_TRUE( std::is_move_assignable<LockProxy<MyData>>::value );
-
-  EXPECT_EQ( md_.locks_, 0 );
-  {
-    auto p1 = md_.withLock();
-    EXPECT_EQ( md_.locks_, 1 );
-    auto p2 = std::move(p1);
-    EXPECT_EQ( md_.locks_, 1 );
-    p1 = std::move(p2);
-    EXPECT_EQ( md_.locks_, 1 );
-  }
-  EXPECT_EQ( md_.locks_, 0 );
-}
-
-
-TEST_F(ButThreadingLockProxyProvider, MoveAsignmentReleasesPreviousLockWhenOwned)
-{
-  EXPECT_EQ( md_.locks_, 0 );
-  MyData other;
-  {
-    auto p1 = other.withLock();
-    auto p2 = md_.withLock();
-    EXPECT_EQ( other.locks_, 1 );
-    EXPECT_EQ( md_.locks_, 1 );
-    p1 = std::move(p2);
-    EXPECT_EQ( other.locks_, 0 );
-    EXPECT_EQ( md_.locks_, 1 );
-  }
-  EXPECT_EQ( other.locks_, 0 );
-  EXPECT_EQ( md_.locks_, 0 );
-}
-
-
-TEST_F(ButThreadingLockProxyProvider, LockSelfAssignmentIsNoOp)
-{
-  EXPECT_EQ( md_.locks_, 0 );
-  {
-    auto p1 = md_.withLock();
-    EXPECT_EQ( md_.locks_, 1 );
-    auto&& p2 = std::move(p1);
-    p1 = std::move(p2);
-    EXPECT_EQ( md_.locks_, 1 );
-  }
-  EXPECT_EQ( md_.locks_, 0 );
-}
-
-
-TEST_F(ButThreadingLockProxyProvider, ProxyIsNonCopyable)
-{
-  EXPECT_FALSE( std::is_copy_constructible<LockProxy<MyData>>::value );
-  EXPECT_FALSE( std::is_copy_assignable<LockProxy<MyData>>::value );
 }
 
 }
