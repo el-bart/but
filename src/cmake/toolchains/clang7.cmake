9b3334a3a66c5aadbb8171eb0edb91e5904096c2
bartek 'basz' szurgot <git@baszerr.eu>
2018-12-22 22:49:01 +0100

    fixed paths for clang-7 and its tools

--- src/cmake/toolchains/clang7.cmake
+++ src/cmake/toolchains/clang7.cmake
@@ -1,10 +1,10 @@
 SET(CMAKE_SYSTEM_NAME Linux)
 
-set(CMAKE_C_COMPILER   clang-7.0)
-set(CMAKE_CXX_COMPILER clang++-7.0)
+set(CMAKE_C_COMPILER   clang-7)
+set(CMAKE_CXX_COMPILER clang++-7)
 
-set(CMAKE_AR      "llvm-ar-7.0"      CACHE FILEPATH "ar")
-set(CMAKE_LINKER  "llvm-ld-7.0"      CACHE FILEPATH "ld")
-set(CMAKE_NM      "llvm-nm-7.0"      CACHE FILEPATH "nm")
-set(CMAKE_OBJDUMP "llvm-objdump-7.0" CACHE FILEPATH "objdump")
-set(CMAKE_RANLIB  "llvm-ranlib-7.0"  CACHE FILEPATH "ranlib")
+set(CMAKE_AR      "llvm-ar-7"      CACHE FILEPATH "ar")
+set(CMAKE_LINKER  "llvm-ld-7"      CACHE FILEPATH "ld")
+set(CMAKE_NM      "llvm-nm-7"      CACHE FILEPATH "nm")
+set(CMAKE_OBJDUMP "llvm-objdump-7" CACHE FILEPATH "objdump")
+set(CMAKE_RANLIB  "llvm-ranlib-7"  CACHE FILEPATH "ranlib")
