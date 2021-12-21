18d83eb3d3b5965308fdb157a861611b099c6546
bartek 'basz' szurgot <git@baszerr.eu>
2021-12-21 22:20:57 +0100

    added remaining toolchains

---
+++ src/cmake/toolchains/gcc10.cmake
@@ -0,0 +1,10 @@
+SET(CMAKE_SYSTEM_NAME Linux)
+
+set(CMAKE_C_COMPILER   gcc-10)
+set(CMAKE_CXX_COMPILER g++-10)
+
+set(CMAKE_AR      "ar"      CACHE FILEPATH "ar")
+set(CMAKE_LINKER  "ld"      CACHE FILEPATH "ld")
+set(CMAKE_NM      "nm"      CACHE FILEPATH "nm")
+set(CMAKE_OBJDUMP "objdump" CACHE FILEPATH "objdump")
+set(CMAKE_RANLIB  "ranlib"  CACHE FILEPATH "ranlib")
