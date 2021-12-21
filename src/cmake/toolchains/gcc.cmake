a8382829517d4ae115dbf48b0bd50c272c474401
bartek 'basz' szurgot <git@baszerr.eu>
2021-12-21 22:03:22 +0100

    default compilers are now in a different target

--- src/cmake/toolchains/gcc.cmake
+++ src/cmake/toolchains/gcc.cmake
@@ -1,1 +1,10 @@
-gcc8.cmake
+SET(CMAKE_SYSTEM_NAME Linux)
+
+set(CMAKE_C_COMPILER   gcc)
+set(CMAKE_CXX_COMPILER g++)
+
+set(CMAKE_AR      "ar"      CACHE FILEPATH "ar")
+set(CMAKE_LINKER  "ld"      CACHE FILEPATH "ld")
+set(CMAKE_NM      "nm"      CACHE FILEPATH "nm")
+set(CMAKE_OBJDUMP "objdump" CACHE FILEPATH "objdump")
+set(CMAKE_RANLIB  "ranlib"  CACHE FILEPATH "ranlib")
