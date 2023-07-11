2a6cd971e442752fdcc763c6cddd1434e6713e7a
bartek 'basz' szurgot <git@baszerr.eu>
2023-07-11 20:45:16 +0200

    in case of latest GCC we disable warning for an expected error condition in manual test

--- src/But/detail/testAddressSanitizer.manual.cpp
+++ src/But/detail/testAddressSanitizer.manual.cpp
@@ -8,6 +8,16 @@ int main()
   memcpy(invalid, "oops", 5);
   std::cout << invalid << std::endl;
   delete [] invalid;
+
+#if __GNUC__ >= 12
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wuse-after-free"
+#endif
+  // this is use-after-free and it's expected, as this test shall ensure it's detected
   *invalid = '?';
+#if __GNUC__ >= 12
+#pragma GCC diagnostic pop
+#endif
+
   std::cerr << "NOTE: status code of an application should be non-zero!" << std::endl;
 }
