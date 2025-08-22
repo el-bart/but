ca6e85056376460e678e20674f765e32c1c0ab47
bartek 'basz' szurgot <git@baszerr.eu>
2025-08-22 21:22:32 +0200

    added minimal test to ensure catch2 will not fail MTs

--- src/But/placeholder.mt.cpp
+++ src/But/placeholder.mt.cpp
@@ -1,1 +1,13 @@
 // TODO: remove once there's a real test with such a file
+#include <But/detail/catch2.hpp>
+
+namespace
+{
+
+TEST_CASE("But::placholder")
+{
+  // there must be at least one test, otherwise Catch2 v3 will fail the run
+  REQUIRE(1 == 1);
+}
+
+}
