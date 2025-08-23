b6d7d69c89d19787850560bbcdf41acda1ca7eb2
bartek 'basz' szurgot <git@baszerr.eu>
2025-08-23 14:42:07 +0200

    added workarounds from GCC 12 to 13 and 14

---
+++ src/cmake/flags/gcc14.cmake
@@ -0,0 +1,7 @@
+# there's a bug in GCC 14:
+# https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105562
+# if optimizations are enabled incorrect "may be used uninitialized" warnings comming up from standard library's internals.
+if("${CMAKE_BUILD_TYPE}" MATCHES "^Release|Relwithdebinfo|Profile$")
+  message(WARNING "BUT: disabling -Wmaybe-uninitialized for buggy GCC version")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-maybe-uninitialized")
+endif()
